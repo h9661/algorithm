00f27b3aefebc0fc12a776278dc40446eafbe783
guscksdn123@gmail.com <guscksdn123@gmail.com>
2021-08-05 21:07:09 +0900

    다시 복구해봄 4

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,88 +1,50 @@
 #include <bits/stdc++.h>
-#define ULL unsigned long long
-#define LL long long int
-#define UI unsigned int
+#define ull unsigned long long
+#define ll long long int
+#define ui unsigned int
 #define endl '\n'
-#define FASTIO (cin.tie(NULL), cout.sync_with_stdio(false), cout.tie(NULL))
-#pragma warning(disable : 4996)
+#define fastio (cin.tie(NULL), cout.sync_with_stdio(false), cout.tie(NULL))
+#define ii pair<int, int>
 using namespace std;
 
-const int MAX = 1000000 + 1;
-int arr[MAX];
-int check[MAX];
-int lis[MAX];
-int idxArr[MAX];
-
-int binary_search(int left, int right, int target) {
-	int mid;
-
-	while (left < right) {
-		mid = (left + right) / 2;
-
-		if (lis[mid] < target)
-			left = mid + 1;
-		else
-			right = mid;
-	}
-
-	return right;
-}
-
-bool cmp(pair<int, int> a, pair<int, int> b) {
-	return a.first < b.first;
-}
+const int MAX = 100 + 1;
+vector<ii> val_cost_pair;
+int val[MAX];
+int cost[MAX];
+int dp[MAX][100001];
 
 int main() {
-	FASTIO;
-	vector<pair<int, int>> pairArr;
-	int N;
-	cin >> N;
+	int N, M;
+	cin >> N >> M;
 
-	for (int i = 0; i < N; i++) {
-		int A, B;
-		cin >> A >> B;
-		pairArr.push_back({ A, B });
-	}
+	val_cost_pair.push_back({ 0, 0 });
 
-	sort(pairArr.begin(), pairArr.end(), cmp);
+	for (int i = 1; i <= N; i++)
+		cin >> val[i];
 
-	for (int i = 0; i < N; i++)
-		arr[i] = pairArr[i].second;
+	for (int i = 1; i <= N; i++)
+		cin >> cost[i];
 
-	int j = 0;
-	int i = 1;
-	lis[0] = arr[0];
-	idxArr[0] = 0;
+	for (int i = 1; i <= N; i++)
+		val_cost_pair.push_back({ val[i], cost[i] });
 
-	while (i < N) {
-		if (lis[j] < arr[i]) {
-			lis[j + 1] = arr[i];
-			idxArr[i] = j + 1;
-			j += 1;
-		}
-		else {
-			int idx = binary_search(0, j, arr[i]);
-			lis[idx] = arr[i];
-			idxArr[i] = idx;
-		}
 
-		i += 1;
-	}
-
-	int k = j;
-	cout << N - (j + 1) << endl;
-
-	for (int i = N - 1; i >= 0; i--) {
-		if (idxArr[i] == k) {
-			check[arr[i]] = true;
-			k--;
-			continue;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= 10001; j++) {
+			if (j - val_cost_pair[i].second >= 0)
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - val_cost_pair[i].second] + val_cost_pair[i].first);
+			else
+				dp[i][j] = dp[i - 1][j];
 		}
 	}
 
-	for (int i = 0; i < N; i++) {
-		if (check[pairArr[i].second] == false)
-			cout << pairArr[i].first << endl;
+	for (int j = 1; j <= 10001; j++) {
+		for (int i = 1; i <= N; i++) {
+			if (dp[i][j] >= M) {
+				cout << j << endl;
+				return 0;
+			}
+		}
 	}
 
 	return 0;

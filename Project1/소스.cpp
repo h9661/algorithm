645fd599bade04193c538d29a8432a65a85c9d05
guscksdn123@gmail.com <guscksdn123@gmail.com>
2021-08-06 11:52:36 +0900

    2021 8 6 14728

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -7,45 +7,30 @@
 #define ii pair<int, int>
 using namespace std;
 
-const int MAX = 100 + 1;
-vector<ii> val_cost_pair;
-int val[MAX];
+// น้มุ 14728 kanpsack dp ps 2021 8 6
+
+const int MAX = 101;
+int N, T;
 int cost[MAX];
-int dp[MAX][100001];
+int value[MAX];
+int dp[MAX][10001];
 
 int main() {
-	int N, M;
-	cin >> N >> M;
-
-	val_cost_pair.push_back({ 0, 0 });
-
-	for (int i = 1; i <= N; i++)
-		cin >> val[i];
-
-	for (int i = 1; i <= N; i++)
-		cin >> cost[i];
-
-	for (int i = 1; i <= N; i++)
-		val_cost_pair.push_back({ val[i], cost[i] });
-
+	cin >> N >> T;
 
 	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= 10001; j++) {
-			if (j - val_cost_pair[i].second >= 0)
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - val_cost_pair[i].second] + val_cost_pair[i].first);
+		cin >> cost[i];
+		cin >> value[i];
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= T; j++) {
+			if (j - cost[i] >= 0)
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - cost[i]] + value[i]);
 			else
 				dp[i][j] = dp[i - 1][j];
 		}
 	}
 
-	for (int j = 1; j <= 10001; j++) {
-		for (int i = 1; i <= N; i++) {
-			if (dp[i][j] >= M) {
-				cout << j << endl;
-				return 0;
-			}
-		}
-	}
-
-	return 0;
+	cout << dp[N][T] << endl;
 }

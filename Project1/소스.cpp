c5581341c29d5c7b566b4d87ac44f3ca09d8c1f0
guscksdn123@gmail.com <guscksdn123@gmail.com>
2021-08-10 19:11:42 +0900

    2021/8/10 백준 1005 처음 풀어보는 위상정렬 문제

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -7,61 +7,73 @@
 #define ii pair<int, int>
 using namespace std;
 
-// 백준 4485
-// 2차원 배열을 이용한 다익스트라 알고리즘
+// 백준 1005
+// 위상 정렬 문제
 
-int N;
-const int MAX = 125 + 1;
-const int INF = 50000;
-int graph[MAX][MAX];
-int check[MAX][MAX];
-int Y[4] = { 1, -1, 0, 0 };
-int X[4] = { 0, 0, -1, 1 };
-
-void bfs() {
-	queue<ii> q;
-	q.push({ 1, 1 });
-	check[1][1] = graph[1][1];
-
-	while (!q.empty()) {
-		int currentY = q.front().first;
-		int currentX = q.front().second;
-
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int nextY = Y[i] + currentY;
-			int nextX = X[i] + currentX;
-
-			if (nextY >= 1 && nextY <= N && nextX >= 1 && nextX <= N) {
-				if (check[nextY][nextX] > check[currentY][currentX] + graph[nextY][nextX]) {
-					check[nextY][nextX] = check[currentY][currentX] + graph[nextY][nextX];
-					q.push({ nextY, nextX });
-				}
-			}
-		}
-	}
-}
+int t;
+int n, k, w;
+vector<vector<int>> p;
+vector<int> dist, times, indeg;
 
 int main() {
-	int count = 1;
-	while (1) {
-		memset(graph, 0, sizeof(graph));
-		memset(check, INF, sizeof(check));
-		
-		cin >> N;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
 
-		if (N == 0)
-			break;
+	cin >> t;
 
-		for (int i = 1; i <= N; i++)
-			for (int j = 1; j <= N; j++)
-				cin >> graph[i][j];
+	while (t--) {
+		dist.clear();
+		times.clear();
+		indeg.clear();
+		p.clear();
 
-		bfs();
+		cin >> n >> k;
 
-		cout << "Problem " << count << ": ";
-		count++;
-		cout << check[N][N] << endl;
+		dist.resize(n + 1);
+		times.resize(n + 1);
+		indeg.resize(n + 1);
+		p.resize(n + 1);
+
+		for (int i = 1; i < n + 1; i++) {
+			cin >> times[i];
+		}
+
+		for (int i = 0; i < k; i++) {
+			int u, v;
+			cin >> u >> v;
+			p[u].push_back(v);
+			indeg[v]++;
+		}
+
+		cin >> w;
+
+		queue<int> q;
+
+		for (int i = 1; i < n + 1; i++) {
+			if (indeg[i] == 0) {
+				q.push(i);
+				dist[i] = times[i];
+			}
+		}
+
+		while (!q.empty()) {
+			int now = q.front();
+			q.pop();
+
+			if (now == w) 
+				break;
+
+			for (int i = 0; i < p[now].size(); i++) {
+				int next = p[now][i];
+
+				if (--indeg[next] == 0)
+					q.push(next);
+
+				if (dist[next] < dist[now] + times[next])
+					dist[next] = dist[now] + times[next];
+			}
+		}
+
+		cout << dist[w] << '\n';
 	}
 }

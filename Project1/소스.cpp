646ae2038de6756b9eca40bccab77092a17e0813
guscksdn123@gmail.com <guscksdn123@gmail.com>
2021-08-16 18:59:06 +0900

    2021/8/16 BackJoon 1922 최소신장트리 프림 알고리즘 이용

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -7,25 +7,25 @@
 #define ii pair<int, int>
 using namespace std;
 
-const int MAX = 10000 + 1;
+const int MAX = 1000 + 1;
+ll ans = 0;
+int N, M;
+
 vector<ii> graph[MAX];
 bool check[MAX];
-ll ans = 0;
-int V, E;
 
-void prim(int start) {
+void prim(int start){
 	priority_queue<ii, vector<ii>, greater<ii>> pq;
 	pq.push({ 0, start });
 
 	while (!pq.empty()) {
 		int currentCost = pq.top().first;
 		int currentNode = pq.top().second;
-
 		pq.pop();
 
 		if (check[currentNode] == true)
 			continue;
-		
+
 		check[currentNode] = true;
 
 		ans += currentCost;
@@ -42,14 +42,15 @@ void prim(int start) {
 	cout << ans << endl;
 }
 
+
 int main() {
-	cin >> V >> E;
-	
-	for (int i = 0; i < E; i++) {
+	cin >> N >> M;
+
+	for (int i = 0; i < M; i++) {
 		int from, to, cost;
 		cin >> from >> to >> cost;
 
-		graph[from].push_back({ cost, to });
+		graph[from].push_back({ cost , to });
 		graph[to].push_back({ cost, from });
 	}
 

3fb547dcf1976a285b17b20c9b68a3b1a19e8a84
guscksdn123@gmail.com <guscksdn123@gmail.com>
2021-08-17 20:32:22 +0900

    2021/8/17 BackJoon 5585 간단한 그리디 문제였다.

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -7,62 +7,24 @@
 #define ii pair<ll, ll>
 using namespace std;
 
-int m, n;
-const int MAX = 200000 + 1;
-vector<ii> graph[MAX];
-bool check[MAX];
-ll ans = 0;
-
-void prim() {
-	priority_queue<ii, vector<ii>, greater<ii>> pq;
-	pq.push({ 0, 0 });
-
-	while (!pq.empty()) {
-		int currentCost = pq.top().first;
-		int currentNode = pq.top().second;
-		pq.pop();
-
-		if (check[currentNode] == true)
-			continue;
-
-		check[currentNode] = true;
-		ans += currentCost;
-
-		for (int i = 0; i < graph[currentNode].size(); i++) {
-			int nextCost = graph[currentNode][i].first;
-			int nextNode = graph[currentNode][i].second;
-
-			if (check[nextNode] == false)
-				pq.push({ nextCost, nextNode });
-		}
-	}
-}
-
 int main() {
-	while (1) {
-		ans = 0;
-		fill(check, check + MAX, false);
-		for (int i = 0; i < MAX; i++)
-			graph[i].clear();
+	int n;
+	cin >> n;
 
-		cin >> m >> n;
+	int k = 1000 - n;
 
-		if (m == 0 && n == 0)
-			break;
+	int arr[6] = { 500, 100, 50, 10, 5, 1 };
 
-		ll totalCost = 0;
+	int count = 0;
 
-		for (int i = 0; i < n; i++) {
-			int from, to, val;
-			cin >> from >> to >> val;
-
-			graph[from].push_back({ val, to });
-			graph[to].push_back({ val, from });
-			totalCost += val;
+	for (int i = 0; i < 6; i++) {
+		while (k >= arr[i]) {
+			k -= arr[i];
+			count++;
 		}
-
-		prim();
-
-		cout << totalCost - ans << endl;
 	}
+
+	cout << count << endl;
+
+	return 0;
 }

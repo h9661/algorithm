6098f0152b7e64582869432f74647862b341153e
guscksdn123@gmail.com <guscksdn123@gmail.com>
2021-08-19 19:28:06 +0900

    2021/8/19 BackJoon 브루트 포스와 다이나믹 프로그래밍으로 푸는 문제였다. 어려웠다. 삼성 문제라 그런지 쫌 난이도가 있었다;;

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,28 +4,42 @@
 #define ui unsigned int
 #define endl '\n'
 #define fastio (cin.tie(NULL), cout.sync_with_stdio(false), cout.tie(NULL))
-#define ii pair<ll, ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
 using namespace std;
+
+const int MAX = 15 + 1;
+int dp[MAX];
+int timeArr[MAX];
+int priceArr[MAX];
 
 int main() {
 	int N;
 	cin >> N;
 
-	vector<int> arr;
-	arr.resize(N);
+	for (int i = 1; i <= N; i++) {
+		cin >> timeArr[i] >> priceArr[i];
+		dp[i] = priceArr[i];
+	}
 
-	for (int i = 0; i < N; i++)
-		cin >> arr[i];
-
-	sort(arr.begin(), arr.end());
+	for (int i = 2; i <= N; i++) {
+		for (int j = 1; j < i; j++) {
+			if (timeArr[j] + j <= i) {
+				dp[i] = max(priceArr[i] + dp[j], dp[i]);
+			}
+		}
+	}
 
 	int ans = 0;
 
-	for (int i = 0; i < N; i++) {
-		if (arr[i] * (N - i) >= arr[N - 1]) {
-			ans = max(ans, arr[i] * (N - i));
+	for (int i = 1; i <= N; i++) {
+		if (i + timeArr[i] - 1 <= N) {
+			if (ans < dp[i])
+				ans = dp[i];
 		}
 	}
 
 	cout << ans << endl;
+
+	return 0;
 }

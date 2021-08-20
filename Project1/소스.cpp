8aea69e05bf17698bb31483dfdc5aaaa5bab4b4e
guscksdn123@gmail.com <guscksdn123@gmail.com>
2021-08-20 18:46:35 +0900

    2021/8/20 BackJoon 10815 정렬과 이분탐색 기초문제.

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,38 +8,46 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 15 + 1;
-int dp[MAX];
-int timeArr[MAX];
-int priceArr[MAX];
+const int MAX = 500000 + 1;
+int N, M;
+int arr1[MAX];
+int arr2[MAX];
 
 int main() {
-	int N;
+	fastio;
 	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> arr1[i];
 
-	for (int i = 1; i <= N; i++) {
-		cin >> timeArr[i] >> priceArr[i];
-		dp[i] = priceArr[i];
-	}
+	cin >> M;
+	for (int i = 0; i < M; i++)
+		cin >> arr2[i];
 
-	for (int i = 2; i <= N; i++) {
-		for (int j = 1; j < i; j++) {
-			if (timeArr[j] + j <= i) {
-				dp[i] = max(priceArr[i] + dp[j], dp[i]);
-			}
+	sort(arr1, arr1 + N);
+
+	int count = 0;
+
+	for (int i = 0; i < M; i++) {
+		int start = 0;
+		int end = N;
+		bool flag = false;
+
+		while (start <= end) {
+			int mid = (start + end) / 2;
+
+			if (arr1[mid] < arr2[i])
+				start = mid + 1;
+			else if(arr1[mid] >= arr2[i])
+				end = mid - 1;
+
+			if (arr1[mid] == arr2[i])
+				flag = true;
 		}
+		if (flag)
+			cout << "1" << " ";
+		else
+			cout << "0" << " ";
 	}
-
-	int ans = 0;
-
-	for (int i = 1; i <= N; i++) {
-		if (i + timeArr[i] - 1 <= N) {
-			if (ans < dp[i])
-				ans = dp[i];
-		}
-	}
-
-	cout << ans << endl;
 
 	return 0;
 }

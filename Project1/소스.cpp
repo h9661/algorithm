bbd0ebcebb7965061872613355d49c31ba9702cd
guscksdn123@gmail.com <guscksdn123@gmail.com>
2021-08-23 16:08:28 +0900

    BackJoon 1613 플로이드 와샬 알고리즘 이것도 플로이드 와샬을 이용하는 문제였다. 쉬웠다.

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,7 +8,7 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 100 + 1;
+const int MAX = 400 + 1;
 const int INF = 2e9;
 ll arr[MAX][MAX];
 
@@ -30,7 +30,7 @@ int main() {
 
 	for (int i = 0; i < M; i++) {
 		int big, small;
-		cin >> big >> small;
+		cin >> small >> big;
 
 		arr[small][big] = 1;
 	}
@@ -44,22 +44,29 @@ int main() {
 		}
 	}
 
-	vector<int> ans(N + 1, 0);
+	int s;
+	cin >> s;
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			if (i != j) {
-				if (arr[i][j] != INF) {
-					ans[j]++;
-					ans[i]++;
-				}
-			}
-		}
+	vector<pii> store;
+
+	for (int i = 0; i < s; i++) {
+		int a, b;
+		cin >> a >> b;
+		store.push_back({ a, b });
 	}
 
-	for (int i = 1; i <= N; i++) {
-		cout << N - ans[i] - 1 << endl;
+	for (int i = 0; i < store.size(); i++) {
+		int fir = store[i].first;
+		int sec = store[i].second;
+
+		if (arr[fir][sec] != INF)
+			cout << "-1" << endl;
+		else if (arr[sec][fir] != INF)
+			cout << "1" << endl;
+		else
+			cout << "0" << endl;
 	}
+
 
 	return 0;
 }

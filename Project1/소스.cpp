e6f39ce5b78c0c1cdba56012e205cb4c5692bf97
guscksdn123@gmail.com <guscksdn123@gmail.com>
2021-08-24 14:54:32 +0900

    BackJoon 1717 분리 집합 어렵지 않다. 분리집합의 특징을 좀 더 알아봐야 겠다

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,45 +8,50 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 500000 + 1;
 int n, m;
+const int MAX = 1000000 + 1;
 int root[MAX];
 
-int _find(int x) {
-	if (root[x] == x) 
+int do_find(int x) {
+	if (root[x] == x)
 		return x;
-
-	return root[x] = _find(root[x]);
+	return root[x] = do_find(root[x]);
 }
 
-void _union(int x, int y) {
-	int rx = _find(x);
-	int ry = _find(y);
+void do_union(int x, int y) {
+	int rootX = do_find(x);
+	int rootY = do_find(y);
 
-	if (rx < ry)
-		root[ry] = rx;
+	if (rootX < rootY)
+		root[rootY] = rootX;
 	else
-		root[rx] = ry;
+		root[rootX] = rootY;
 }
 
 int main() {
+	fastio;
+	// 0 a b -> a포함 집합 b 포함 집합 union
+	// 1 a b -> a와 b의 루트가 같나 ? YES : NO
 	cin >> n >> m;
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i <= n; i++)
 		root[i] = i;
 
-	for (int i = 1; i <= m; i++) {
-		int u, v;
-		cin >> u >> v;
-		if (_find(u) == _find(v)) {
-			cout << i << endl;
-			return 0;
-		}
-		else
-			_union(u, v);
-	}
+	for (int i = 0; i < m; i++) {
+		int op, a, b;
+		cin >> op >> a >> b;
 
-	cout << "0" << endl;
+		if (op == 0) {
+			do_union(a, b);
+		}
+		else {
+			if (do_find(a) == do_find(b))
+				cout << "YES" << endl;
+			else
+				cout << "NO" << endl;
+		}
+	}
+	
 
 	return 0;
 }

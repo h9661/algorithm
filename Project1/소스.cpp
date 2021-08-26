8a8c5eba1512dd52bf75536d19078e5309b89dba
guscksdn123@gmail.com <guscksdn123@gmail.com>
2021-08-26 14:26:42 +0900

    BackJoon 17845 KnapSack

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,64 +8,29 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-int N, M;
-
-const int MAX = 300 + 1;
-int costs[MAX][MAX];
-int dp[MAX][MAX];
-int path[MAX][MAX];
+const int MAX = 1000 + 1;
+int I[MAX];
+int T[MAX];
+int dp[1001][10001];
 
 int main() {
-	cin >> N >> M;
+	int N, K;
+	cin >> N >> K;
 
-	for (int i = 1; i <= N; i++) {
-		int n;
-		cin >> n;
-
-		for (int j = 1; j <= M; j++)
-			cin >> costs[j][i];
+	for (int i = 1; i <= K; i++) {
+		cin >> I[i] >> T[i];
 	}
 
-	for (int i = 1; i <= M; i++) {
+	for (int i = 1; i <= K; i++) {
 		for (int j = 1; j <= N; j++) {
-			if (dp[i - 1][j] < costs[i][j]) {
-				dp[i][j] = costs[i][j];
-				path[i][j] = j;
+			if (j - T[i] >= 0) {
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - T[i]] + I[i]);
 			}
 			else {
-				dp[i][j] = dp[i - 1][j];
-				path[i][j] = 0;
-			}
-
-			int k = 1;
-			while (k < j) {
-				if (dp[i][j] < dp[i - 1][k] + costs[i][j - k]) {
-					dp[i][j] = dp[i - 1][k] + costs[i][j - k];
-					path[i][j] = j - k;
-				}
-				k++;
+				dp[i][j] = max(dp[i - 1][j], dp[i][j]);
 			}
 		}
 	}
 
-	cout << dp[M][N] << endl;
-
-	vector<int> store;
-	int temp = path[M][N];
-	int k = N;
-	store.push_back(temp);
-
-	for (int i = M - 1; i >= 1; i--) {
-		k -= temp;
-		temp = path[i][k];
-		store.push_back(temp);
-	}
-
-	reverse(store.begin(), store.end());
-
-	for (auto i : store)
-		cout << i << " ";
-
-
-	return 0;
+	cout << dp[K][N] << endl;
 }

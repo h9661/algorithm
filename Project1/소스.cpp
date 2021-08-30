9cfea9c8b98187443b165d4c8a60d274e51a618c
guscksdn123@gmail.com <guscksdn123@gmail.com>
2021-08-30 11:30:12 +0900

    Revert "BackJoon 투포인터 + 브루트포스"
    This reverts commit c5464604332e3fbf516e0b236277613ee9327e18.

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,66 +8,36 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 5000 + 1;
-ll arr[MAX];
-
 int main() {
-	int N;
-	cin >> N;
+	int N, K;
+	cin >> N >> K;
 
-	for (int i = 0; i < N; i++)
-		cin >> arr[i];
+	queue<int> q;
+	vector<int> store;
+	int count = 1;
 
-	sort(arr, arr + N);
+	for (int i = 1; i <= N; i++)
+		q.push(i);
 
-	int left = 0;
-	int right = N - 1;
-
-	ll minimum = 4e9;
-
-	vector<ll> store;
-	ll temp1 = arr[0];
-	ll temp2 = arr[1];
-	ll temp3 = arr[2];
-
-	for (int i = 0; i < N; i++) {
-		left = 0;
-		right = N - 1;
-
-		while (left < right) {
-			ll sum = arr[left] + arr[right] + arr[i];
-
-			if (abs(sum) < minimum) {
-				if (i == left || i == right) {
-					if (sum < 0)
-						left += 1;
-					else
-						right -= 1;
-					continue;
-				}
-
-				minimum = abs(sum);
-				temp1 = arr[left];
-				temp2 = arr[right];
-				temp3 = arr[i];
-			}
-
-			if (sum < 0)
-				left += 1;
-			else
-				right -= 1;
-
+	while (!q.empty()) {
+		if (count == K) {
+			store.push_back(q.front());
+			q.pop();
+			count = 1;
+		}
+		else {
+			q.push(q.front());
+			q.pop();
+			count++;
 		}
 	}
 
-	store.push_back(temp1);
-	store.push_back(temp2);
-	store.push_back(temp3);
 
-	sort(store.begin(), store.end());
-
-	for (auto i : store)
-		cout << i << " ";
+	cout << "<";
+	for (int i = 0; i < store.size() - 1; i++)
+		cout << store[i] << ", ";
+	cout << store[store.size() - 1] << ">";
 
 	return 0;
+
 }

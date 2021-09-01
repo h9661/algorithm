4a9261a19a7927c776b0dc09563e739d3308e1b8
h9661 <guscksdn123@gmail.com>
2021-09-01 13:36:02 +0900

    BackJoon 9470 위상 정렬

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,55 +8,69 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 100 + 1;
-vector<vector<pll>> graph;
-vector<int> indeg;
-int N, M;
-int countArr[MAX][MAX];
+const int MAX = 1000 + 1;
+vector<int> graph[MAX];
+int indeg[MAX];
+int order[MAX];
+int orderCount[MAX];
 
 int main() {
-	cin >> N >> M;
+	int tt;
+	cin >> tt;
+	
+	int k = 1;
 
-	indeg.resize(N + 1);
-	graph.resize(N + 1);
+	while (tt--) {
+		int K, M, P;
+		cin >> K >> M >> P;
 
-	for (int i = 0; i < M; i++) {
-		int X, Y, K;;
-		cin >> X >> Y >> K;
+		for (int i = 0; i < MAX; i++)
+			graph[i].clear();
 
-		graph[Y].push_back({ X, K });
-		indeg[X]++;
-	}
+		fill(indeg, indeg + MAX, 0);
+		fill(order, order + MAX, 0);
+		fill(orderCount, orderCount + MAX, 0);
 
-	queue<int> q;
-	for (int i = 1; i <= N; i++) {
-		if (indeg[i] == 0) {
-			q.push(i);
-			countArr[i][i] = 1;
+		for (int i = 0; i < P; i++) {
+			int u, v;
+			cin >> u >> v;
+
+			graph[u].push_back(v);
+			indeg[v]++;
 		}
-	}
-
-	while (!q.empty()) {
-		int now = q.front();
-		q.pop();
-
-		for (int i = 0; i < graph[now].size(); i++) {
-			int next = graph[now][i].first;
-			int num = graph[now][i].second;
-
-			for (int j = 1; j <= N; j++)
-				countArr[next][j] += countArr[now][j] * num;
-			
-			if (--indeg[next] == 0)
-				q.push(next);
+		
+		queue<int> q;
+		for (int i = 1; i <= M; i++) {
+			if (indeg[i] == 0) {
+				q.push(i);
+				order[i] = 1;
+			}
 		}
+
+		while (!q.empty()) {
+			int now = q.front();
+			q.pop();
+
+			for (int i = 0; i < graph[now].size(); i++) {
+				int next = graph[now][i];
+
+				// 순서가 크면 바꿔주고 카운트 1
+				if (order[now] > order[next]) {
+					order[next] = order[now];
+					orderCount[next] = 1;
+				}
+				else if (order[now] == order[next])
+					orderCount[next]++;
+
+				if (--indeg[next] == 0) {
+					if (orderCount[next] >= 2)
+						order[next] = order[next] + 1;
+
+					q.push(next);
+				}
+			}
+		}
+		
+		cout << K << " " << *max_element(order, order + M + 1) << endl;
 	}
-
-
-	for (int i = 1; i <= N; i++) {
-		if (countArr[N][i] != 0)
-			cout << i << " " << countArr[N][i] << endl;
-	}
-
-	return 0;
 }

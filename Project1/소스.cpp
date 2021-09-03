4b2801ff36f042df41ddc872e111753839304dde
h9661 <guscksdn123@gmail.com>
2021-09-03 16:22:28 +0900

    BackJoon 17472 MST + BFS + 브루트포스

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,74 +8,197 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 1000 + 1;
-vector<pair<double, int>> graph[MAX];
-pair<int, int> coordinate[MAX];
-bool check[MAX];
+int islandMap[15][15];
+bool check[15][15];
 int N, M;
+int Y[4] = { -1, 1, 0, 0 };
+int X[4] = { 0, 0, -1, 1 };
 
-double FindDistance(double x1, double y1, double x2, double y2) {
-	double dx = (x2 - x1) * (x2 - x1);
-	double dy = (y2 - y1) * (y2 - y1);
+int island_num = 1;
 
-	return sqrt(dx + dy);
+const int MAX = 10;
+vector<pii> graph[MAX];
+bool pqCheck[MAX];
+
+void bfs(int i,int j) {
+	queue<pii> q;
+	q.push({ i, j });
+	check[i][j] = true;
+	islandMap[i][j] = island_num;
+
+	while (!q.empty()) {
+		int currentY = q.front().first;
+		int currentX = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nextY = Y[i] + currentY;
+			int nextX = X[i] + currentX;
+
+			if (nextY >= 1 && nextY <= N && nextX >= 1 && nextX <= M && check[nextY][nextX] == false && islandMap[nextY][nextX] != 0) {
+				q.push({ nextY, nextX });
+				check[nextY][nextX] = true;
+				islandMap[nextY][nextX] = island_num;
+			}
+		}
+	}
+
+	island_num++;
+}
+
+pii IslandSearchLeft(int i, int j, int startIsland) {
+	int count = 0;
+
+	for (int pos = j - 1; pos >= 1; pos--) {
+		if (islandMap[i][pos] == 0) {
+			count++;
+			continue;
+		}
+
+		if (islandMap[i][pos] == startIsland)
+			return { -1, -1 };
+		else
+			return { count, islandMap[i][pos] };
+	}
+
+	return { -1, -1 };
+}
+
+pii IslandSearchRight(int i, int j, int startIsland) {
+	int count = 0;
+
+	for (int pos = j + 1; pos <= M; pos++) {
+		if (islandMap[i][pos] == 0) {
+			count++;
+			continue;
+		}
+
+		if (islandMap[i][pos] == startIsland)
+			return{ -1, -1 };
+		else
+			return { count,islandMap[i][pos] };
+	}
+
+	return { -1, -1 };
+}
+
+pii IsIandSearchUp(int i, int j, int startIsland) {
+	int count = 0;
+
+	for (int pos = i - 1; pos >= 1; pos--) {
+		if (islandMap[pos][j] == 0) {
+			count++;
+			continue;
+		}
+
+		if (islandMap[pos][j] == startIsland)
+			return { -1, -1 };
+		else
+			return { count, islandMap[pos][j] };
+	}
+
+	return { -1, -1 };
+}
+
+pii IslandSearchDown(int i, int j, int startIsland) {
+	int count = 0;
+
+	for (int pos = i + 1; pos <= N; pos++) {
+		if (islandMap[pos][j] == 0) {
+			count++;
+			continue;
+		}
+
+		if (islandMap[pos][j] == startIsland)
+			return { -1, -1 };
+		else
+			return { count, islandMap[pos][j] };
+	}
+
+	return { -1, -1 };
 }
 
 int main() {
 	cin >> N >> M;
 
+
+/*──── 섬 번호 부여하기 ─────*/
 	for (int i = 1; i <= N; i++) {
-		int u, v;
-		cin >> u >> v;
-
-		coordinate[i].first = u;
-		coordinate[i].second = v;
-	}
-
-	for (int i = 1; i <= M; i++) {
-		int u, v;
-		cin >> u >> v;
-
-		graph[u].push_back({ 0.0, v });
-		graph[v].push_back({ 0.0, u });
+		for (int j = 1; j <= M; j++)
+			cin >> islandMap[i][j];
 	}
 
 	for (int i = 1; i <= N; i++) {
-		for (int j = i + 1; j <= N; j++) {
-			double dist = FindDistance(coordinate[i].first, coordinate[i].second, coordinate[j].first, coordinate[j].second);
+		for (int j = 1; j <= M; j++) {
+			if (islandMap[i][j] != 0 && check[i][j] == false)
+				bfs(i, j);
+		}
+	}
+//────────────────────
+	// 그래프에 노드와 비용 넣기
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			if (islandMap[i][j] == 0)
+				continue;
 
-			graph[i].push_back({ dist, j });
-			graph[j].push_back({ dist, i });
+			int startIsland = islandMap[i][j];
+
+			pii temp;
+			temp = IslandSearchLeft(i, j, startIsland);
+			if (temp.first > 1)
+				graph[startIsland].push_back({ temp.first, temp.second });
+
+
+			temp = IslandSearchRight(i, j, startIsland);
+			if (temp.first > 1)
+				graph[startIsland].push_back({ temp.first, temp.second });
+
+
+			temp = IsIandSearchUp(i, j, startIsland);
+			if (temp.first > 1)
+				graph[startIsland].push_back({ temp.first, temp.second });
+
+
+			temp = IslandSearchDown(i, j, startIsland);
+			if (temp.first > 1)
+				graph[startIsland].push_back({ temp.first, temp.second });
+
 		}
 	}
 
-	priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> pq;
-	pq.push({ 0.0, 1 });
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	pq.push({ 0, 1 });
 
-	double ans = 0;
+	int ans = 0;
 
 	while (!pq.empty()) {
-		double currentCost = pq.top().first;
-		int currentNode = pq.top().second;
+		int nowCost = pq.top().first;
+		int nowNode = pq.top().second;
 		pq.pop();
 
-		if (check[currentNode] == true)
+		if (pqCheck[nowNode] == true)
 			continue;
+		pqCheck[nowNode] = true;
+		ans += nowCost;
 
-		check[currentNode] = true;
-		ans += currentCost;
+		for (int i = 0; i < graph[nowNode].size(); i++) {
+			int nextCost = graph[nowNode][i].first;
+			int nextNode = graph[nowNode][i].second;
 
-		for (int i = 0; i < graph[currentNode].size(); i++) {
-			double nextCost = graph[currentNode][i].first;
-			int nextNode = graph[currentNode][i].second;
-
-			if (check[nextNode] == false) 
+			if (pqCheck[nextNode] == false)
 				pq.push({ nextCost, nextNode });
 		}
 	}
 
-	cout << fixed;
-	cout.precision(2);
+	for (int i = 1; i <= island_num - 1; i++) {
+		if (pqCheck[i] == false) {
+			cout << "-1" << endl;
+			return 0;
+		}
+	}
 
-	cout << ans << endl;
+	if (ans == 0)
+		cout << "-1" << endl;
+	else
+		cout << ans << endl;
 }

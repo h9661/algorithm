93fde8a886106221a85c4847f31512c30570f995
h9661 <guscksdn123@gmail.com>
2021-09-07 15:51:04 +0900

    BackJoon 14502 BFS + BruteForce

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,55 +8,100 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 1000 + 1;
-vector<pii> graph[MAX];
-bool check[MAX];
-
+int N, M;
+const int MAX = 8 + 1;
 int arr[MAX][MAX];
+bool check[MAX][MAX];
+int Y[4] = { -1, 1, 0, 0 };
+int X[4] = { 0, 0, -1, 1 };
+vector<int> store;
+
+void bfs() {
+	int graph[MAX][MAX];
+	fill(&graph[0][0], &graph[MAX - 1][MAX], 0);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++)
+			graph[i][j] = arr[i][j];
+	}
+
+	queue<pii> q;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (graph[i][j] == 2) {
+				q.push({ i, j });
+				check[i][j] = true;
+			}
+		}
+	}
+
+	while (!q.empty()) {
+		int currentY = q.front().first;
+		int currentX = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nextY = Y[i] + currentY;
+			int nextX = X[i] + currentX;
+
+			if (nextY >= 0 && nextY < N && nextX >= 0 && nextX < M) {
+				if (check[nextY][nextX] == false && graph[nextY][nextX] != 1) {
+					check[nextY][nextX] = true;
+					graph[nextY][nextX] = 2;
+					q.push({ nextY, nextX });
+				}
+			}
+		}
+	}
+
+	int count = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (graph[i][j] == 0)
+				count++;
+		}
+	}
+
+	store.push_back(count);
+}
 
 int main() {
-	int N;
-	cin >> N;
+	fastio;
+	cin >> N >> M;
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++)
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++)
 			cin >> arr[i][j];
 	}
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = i + 1; j <= N; j++) {
-			if (i == j)
-				continue;
-
-			graph[i].push_back({ arr[i][j], j });
-			graph[j].push_back({ arr[i][j], i });
-		}
-	}
-
-	priority_queue<pii, vector<pii>, greater<pii>> pq;
-	pq.push({ 0, 1 });
-
-	ll ans = 0;
-
-	while (!pq.empty()) {
-		int currentCost = pq.top().first;
-		int currentNode = pq.top().second;
-		pq.pop();
-
-		if (check[currentNode] == true)
+	for (int a = 0; a < N * M; a++) {
+		if (arr[a / M][a % M] != 0)
 			continue;
 
-		check[currentNode] = true;
-		ans += currentCost;
+		for (int b = a + 1; b < N * M; b++) {
+			if (arr[b / M][b % M] != 0)
+				continue;
 
-		for (int i = 0; i < graph[currentNode].size(); i++) {
-			int nextCost = graph[currentNode][i].first;
-			int nextNode = graph[currentNode][i].second;
+			for (int c = b + 1; c < N * M; c++) {
+				if (arr[c / M][c % M] != 0)
+					continue;
 
-			if (check[nextNode] == false)
-				pq.push({ nextCost, nextNode });
+				fill(&check[0][0], &check[MAX - 1][MAX], false);
+
+				arr[a / M][a % M] = 1;
+				arr[b / M][b % M] = 1;
+				arr[c / M][c % M] = 1;
+
+				bfs();
+
+				arr[a / M][a % M] = 0;
+				arr[b / M][b % M] = 0;
+				arr[c / M][c % M] = 0;
+			}
 		}
 	}
 
-	cout << ans << endl;
+	cout << *max_element(store.begin(), store.end()) << endl;
+	
+	return 0;
 }

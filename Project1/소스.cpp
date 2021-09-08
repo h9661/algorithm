1fffd07b6f4aa83d07fe0797efd48cf725d2530f
h9661 <guscksdn123@gmail.com>
2021-09-08 16:19:44 +0900

    BackJoon 3665 위상정렬

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,100 +8,108 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-int N, M;
-const int MAX = 8 + 1;
-int arr[MAX][MAX];
-bool check[MAX][MAX];
-int Y[4] = { -1, 1, 0, 0 };
-int X[4] = { 0, 0, -1, 1 };
+const int MAX = 500 + 1;
+
+int relation[MAX][MAX];
+int arr[MAX];
+int indeg[MAX];
+vector<int> graph[MAX];
+bool printCheck[MAX];
 vector<int> store;
-
-void bfs() {
-	int graph[MAX][MAX];
-	fill(&graph[0][0], &graph[MAX - 1][MAX], 0);
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++)
-			graph[i][j] = arr[i][j];
-	}
-
-	queue<pii> q;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (graph[i][j] == 2) {
-				q.push({ i, j });
-				check[i][j] = true;
-			}
-		}
-	}
-
-	while (!q.empty()) {
-		int currentY = q.front().first;
-		int currentX = q.front().second;
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int nextY = Y[i] + currentY;
-			int nextX = X[i] + currentX;
-
-			if (nextY >= 0 && nextY < N && nextX >= 0 && nextX < M) {
-				if (check[nextY][nextX] == false && graph[nextY][nextX] != 1) {
-					check[nextY][nextX] = true;
-					graph[nextY][nextX] = 2;
-					q.push({ nextY, nextX });
-				}
-			}
-		}
-	}
-
-	int count = 0;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (graph[i][j] == 0)
-				count++;
-		}
-	}
-
-	store.push_back(count);
-}
 
 int main() {
 	fastio;
-	cin >> N >> M;
+	int tt;
+	cin >> tt;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++)
-			cin >> arr[i][j];
-	}
+	while (tt--) {
+		fill(&relation[0][0], &relation[MAX - 1][MAX], 0);
+		fill(arr, arr + MAX, 0);
+		fill(indeg, indeg + MAX, 0);
+		fill(printCheck, printCheck + MAX, false);
+		for (int i = 0; i < MAX; i++)
+			graph[i].clear();
+		store.clear();
 
-	for (int a = 0; a < N * M; a++) {
-		if (arr[a / M][a % M] != 0)
-			continue;
 
-		for (int b = a + 1; b < N * M; b++) {
-			if (arr[b / M][b % M] != 0)
-				continue;
+		int n;
+		cin >> n;
 
-			for (int c = b + 1; c < N * M; c++) {
-				if (arr[c / M][c % M] != 0)
-					continue;
+		for (int i = 1; i <= n; i++)
+			cin >> arr[i];
 
-				fill(&check[0][0], &check[MAX - 1][MAX], false);
+		for (int i = 1; i <= n; i++) {
+			for (int j = i + 1; j <= n; j++) {
+				int u = arr[i];
+				int v = arr[j];
 
-				arr[a / M][a % M] = 1;
-				arr[b / M][b % M] = 1;
-				arr[c / M][c % M] = 1;
-
-				bfs();
-
-				arr[a / M][a % M] = 0;
-				arr[b / M][b % M] = 0;
-				arr[c / M][c % M] = 0;
+				relation[u][v] = 1;
 			}
 		}
-	}
 
-	cout << *max_element(store.begin(), store.end()) << endl;
-	
-	return 0;
+		int m;
+		cin >> m;
+
+		for (int i = 0; i < m; i++) {
+			int u, v;
+			cin >> u >> v;
+
+			if (relation[u][v] == 1) {
+				relation[u][v] = 0;
+				relation[v][u] = 1;
+			}
+			else if (relation[v][u] = 1) {
+				relation[v][u] = 0;
+				relation[u][v] = 1;
+			}
+		}
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (relation[i][j] == 1) {
+					graph[i].push_back(j);
+					indeg[j]++;
+				}
+			}
+		}
+
+		queue<int> q;
+
+		for (int i = 1; i <= n; i++) {
+			if (indeg[i] == 0)
+				q.push(i);
+		}
+
+		while (!q.empty()) {
+			int currentNode = q.front();
+			q.pop();
+
+			if (printCheck[currentNode] == false) {
+				store.push_back(currentNode);
+				printCheck[currentNode] = true;
+			}
+
+			for (int i = 0; i < graph[currentNode].size(); i++) {
+				int nextNode = graph[currentNode][i];
+
+				if (--indeg[nextNode] == 0)
+					q.push(nextNode);
+			}
+		}
+
+		bool check = true;
+		for (int i = 1; i <= n; i++) {
+			if (printCheck[i] == false)
+				check = false;
+		}
+
+		if (check) {
+			for (auto i : store)
+				cout << i << " ";
+		}
+		else {
+			cout << "IMPOSSIBLE";
+		}
+		cout << endl;
+	}
 }

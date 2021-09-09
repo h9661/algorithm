0186c0267ae6e19f0c65afb285996cf63bf41744
h9661 <guscksdn123@gmail.com>
2021-09-09 16:09:24 +0900

    BackJoon 11660 구간합 + dp

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,108 +8,35 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 500 + 1;
-
-int relation[MAX][MAX];
-int arr[MAX];
-int indeg[MAX];
-vector<int> graph[MAX];
-bool printCheck[MAX];
-vector<int> store;
+const int MAX = 1024 + 1;
+int arr[MAX][MAX];
+int dp[MAX][MAX];
 
 int main() {
 	fastio;
-	int tt;
-	cin >> tt;
+	int N;
+	int M;
+	cin >> N;
+	cin >> M;
 
-	while (tt--) {
-		fill(&relation[0][0], &relation[MAX - 1][MAX], 0);
-		fill(arr, arr + MAX, 0);
-		fill(indeg, indeg + MAX, 0);
-		fill(printCheck, printCheck + MAX, false);
-		for (int i = 0; i < MAX; i++)
-			graph[i].clear();
-		store.clear();
-
-
-		int n;
-		cin >> n;
-
-		for (int i = 1; i <= n; i++)
-			cin >> arr[i];
-
-		for (int i = 1; i <= n; i++) {
-			for (int j = i + 1; j <= n; j++) {
-				int u = arr[i];
-				int v = arr[j];
-
-				relation[u][v] = 1;
-			}
-		}
-
-		int m;
-		cin >> m;
-
-		for (int i = 0; i < m; i++) {
-			int u, v;
-			cin >> u >> v;
-
-			if (relation[u][v] == 1) {
-				relation[u][v] = 0;
-				relation[v][u] = 1;
-			}
-			else if (relation[v][u] = 1) {
-				relation[v][u] = 0;
-				relation[u][v] = 1;
-			}
-		}
-
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				if (relation[i][j] == 1) {
-					graph[i].push_back(j);
-					indeg[j]++;
-				}
-			}
-		}
-
-		queue<int> q;
-
-		for (int i = 1; i <= n; i++) {
-			if (indeg[i] == 0)
-				q.push(i);
-		}
-
-		while (!q.empty()) {
-			int currentNode = q.front();
-			q.pop();
-
-			if (printCheck[currentNode] == false) {
-				store.push_back(currentNode);
-				printCheck[currentNode] = true;
-			}
-
-			for (int i = 0; i < graph[currentNode].size(); i++) {
-				int nextNode = graph[currentNode][i];
-
-				if (--indeg[nextNode] == 0)
-					q.push(nextNode);
-			}
-		}
-
-		bool check = true;
-		for (int i = 1; i <= n; i++) {
-			if (printCheck[i] == false)
-				check = false;
-		}
-
-		if (check) {
-			for (auto i : store)
-				cout << i << " ";
-		}
-		else {
-			cout << "IMPOSSIBLE";
-		}
-		cout << endl;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
+			cin >> arr[i][j];
 	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + arr[i][j];
+		}
+	}
+
+
+	while (M--) {
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+
+		cout << dp[x2][y2] - (dp[x1 - 1][y2] + dp[x2][y1 - 1]) + dp[x1 - 1][y1 - 1] << endl;
+	}
+
+	return 0;
 }

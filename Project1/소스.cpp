4a7de644ff8493a332e96e10c22f28635164cd0b
h9661 <guscksdn123@gmail.com>
2021-09-10 16:28:02 +0900

    BackJoon 6086 최대 유량(네트워크 플로우)

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,35 +8,85 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 1024 + 1;
-int arr[MAX][MAX];
-int dp[MAX][MAX];
+const int INF = 987654321;
+const int MAXN = 52;
+int pipe_num;
+int capacity[MAXN][MAXN], flow[MAXN][MAXN];
+int parent[MAXN];
+vector<int> graph[MAXN];
+
+int MaximumFlow(int source, int sink) {
+    memset(flow, 0, sizeof(flow));
+    int total_flow = 0;
+
+    while (1) {
+        fill(parent, parent + MAXN, -1);
+        queue<int> q;
+        parent[source] = source;
+        q.push(source);
+
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+
+            for (int i = 0; i < graph[x].size(); i++) {
+                int y = graph[x][i];
+
+                if (capacity[x][y] - flow[x][y] > 0 && parent[y] == -1) {
+                    q.push(y);
+                    parent[y] = x;
+
+                    if (y == sink)
+                        break;
+                }
+            }
+        }
+
+        if (parent[sink] == -1)
+            break;
+
+        int amount = INF;
+        for (int p = sink; p != source; p = parent[p])
+            amount = min(capacity[parent[p]][p] - flow[parent[p]][p], amount);
+
+        for (int p = sink; p != source; p = parent[p]) {
+            flow[parent[p]][p] += amount;
+            flow[p][parent[p]] -= amount;
+        }
+
+        total_flow += amount;
+    }
+
+    return total_flow;
+}
 
 int main() {
-	fastio;
-	int N;
-	int M;
-	cin >> N;
-	cin >> M;
+    ios_base::sync_with_stdio(0);
+    cin >> pipe_num;
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++)
-			cin >> arr[i][j];
-	}
+    memset(capacity, 0, sizeof(capacity));
+    char u, v;
+    int pipe_cap;
+    for (int i = 0; i < pipe_num; ++i) {
+        cin >> u >> v >> pipe_cap;
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + arr[i][j];
-		}
-	}
+        if (u > 90)
+            u = u - 'a' + 26;
+        else
+            u = u - 'A';
 
+        if (v > 90)
+            v = v - 'a' + 26;
+        else
+            v = v - 'A';
 
-	while (M--) {
-		int x1, y1, x2, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
 
-		cout << dp[x2][y2] - (dp[x1 - 1][y2] + dp[x2][y1 - 1]) + dp[x1 - 1][y1 - 1] << endl;
-	}
+        capacity[u][v] += pipe_cap;
+        capacity[v][u] += pipe_cap;
+    }
 
-	return 0;
+    cout << MaximumFlow('A' - 'A', 'Z' - 'A') << "\n";
+    return 0;
 }

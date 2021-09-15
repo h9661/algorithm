67ab5aa212db18ec18dc7929833708fd82ac60da
h9661 <guscksdn123@gmail.com>
2021-09-15 15:39:32 +0900

    BackJoon 2316 최대 유량 정점 분할

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -8,7 +8,7 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 400 + 1;
+const int MAX = 800 + 1;
 const int INF = 987654321;
 
 vector<int> graph[MAX];
@@ -16,12 +16,13 @@ int parent[MAX];
 int capacity[MAX][MAX];
 int flow[MAX][MAX];
 
-int maxFlow(int start, int des) {
+int maxFlow(int source, int sink) {
 	int total_flow = 0;
-
+	int start = source, des = sink;
 	while (1) {
 		fill(parent, parent + MAX, -1);
 		queue<int> q;
+
 		q.push(start);
 		parent[start] = start;
 
@@ -35,7 +36,7 @@ int maxFlow(int start, int des) {
 				if (capacity[x][y] - flow[x][y] > 0 && parent[y] == -1) {
 					q.push(y);
 					parent[y] = x;
-
+					
 					if (y == des)
 						break;
 				}
@@ -46,7 +47,7 @@ int maxFlow(int start, int des) {
 			break;
 
 		int amount = INF;
-		for (int i = des; i != start; i = parent[i])
+		for (int i = des; i != start; i = parent[i]) 
 			amount = min(capacity[parent[i]][i] - flow[parent[i]][i], amount);
 
 		for (int i = des; i != start; i = parent[i]) {
@@ -63,18 +64,31 @@ int maxFlow(int start, int des) {
 int main() {
 	int N, P;
 	cin >> N >> P;
+	
+	const int OUT = 400;
+
+	for (int i = 1; i <= N; i++) {
+		graph[i].push_back(i + OUT);
+		graph[i + OUT].push_back(i);
+
+		capacity[i][i + OUT] = 1;
+	}
 
 	for (int i = 0; i < P; i++) {
 		int u, v;
 		cin >> u >> v;
 
-		graph[u].push_back(v);
-		graph[v].push_back(u);
+		graph[u + OUT].push_back(v);
+		graph[v].push_back(u + OUT);
 
-		capacity[u][v] += 1;
+		graph[v + OUT].push_back(u);
+		graph[u].push_back(v + OUT);
+
+		capacity[u + OUT][v] += 1;
+		capacity[v + OUT][u] += 1;
 	}
 
-	int start = 1;
+	int start = 401;
 	int des = 2;
 
 

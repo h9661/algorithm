2d2ffc82c970cd3703b8a02b2c7c13d56cda0551
h9661 <guscksdn123@gmail.com>
2021-09-17 13:24:39 +0900

    BackJoon 2188 이분 매칭

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -9,144 +9,54 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 10000 + 1;
-const int INF = 987654321;
+const int MAX = 400 + 1;
+vector<int> graph[MAX];
+int d[MAX];
+bool c[MAX];
 
-vector<short> graph[MAX];
-vector<short> path[100];
-bool capacity[MAX][MAX];
-bool flow[MAX][MAX];
-short parent[MAX];
-int g_count = 0;
+bool dfs(int x) {
+	for (int i = 0; i < graph[x].size(); i++) {
+		int t = graph[x][i];
 
-int maxFlow(int source, int sink) {
-	int total_flow = 0;
-	int start = source, des = sink;
+		if (c[t])
+			continue;
 
-	while (1) {
-		fill(parent, parent + MAX, -1);
-		queue<int> q;
+		c[t] = true;
 
-		q.push(start);
-		parent[start] = start;
-
-		while (!q.empty()) {
-			int x = q.front();
-			q.pop();
-
-			for (int i = 0; i < graph[x].size(); i++) {
-				int y = graph[x][i];
-
-				if (capacity[x][y] - flow[x][y] > 0 && parent[y] == -1) {
-					q.push(y);
-					parent[y] = x;
-					
-					if (y == des)
-						break;
-				}
-			}
+		if (d[t] == 0 || dfs(d[t])) {
+			d[t] = x;
+			return true;
 		}
-
-		if (parent[des] == -1)
-			break;
-
-
-		int amount = INF;
-		for (int i = des; i != start; i = parent[i]) 
-			amount = min(capacity[parent[i]][i] - flow[parent[i]][i], amount);
-
-		for (int i = des; i != start; i = parent[i]) {
-			flow[parent[i]][i] += amount;
-			flow[i][parent[i]] -= amount;
-
-			if(i < 5000)
-				path[g_count].push_back(i);
-		}
-
-		g_count++;
-		total_flow += amount;
 	}
 
-	return total_flow;
+	return false;
 }
 
 int main() {
-	int tt = 1;
+	int N, M;
+	cin >> N >> M;
 
-	while (1) {
-		cin.clear();
-		for (int i = 0; i < MAX; i++)
-			graph[i].clear();
+	const int ROOM = 200;
 
-		for (int i = 0; i < 100; i++)
-			path[i].clear();
+	for (int i = 1; i <= N; i++) {
+		int k;
+		cin >> k;
 
-		fill(&capacity[0][0], &capacity[MAX - 1][MAX], 0);
-		fill(&flow[0][0], &flow[MAX - 1][MAX], 0);
-		g_count = 0;
+		while (k--) {
+			int temp;
+			cin >> temp;
 
-		int K, N;
-		cin >> K >> N;
-
-		if (K == 0 && N == 0)
-			break;
-
-		const int OUT = 5000;
-		const int IN = 0;
-
-		for (int i = 1; i <= N; i++) {
-			graph[i + IN].push_back(i + OUT);
-			graph[i + OUT].push_back(i + IN);
-
-			capacity[i + IN][i + OUT] = 1;
+			graph[i].push_back(temp + ROOM);
 		}
-
-		for (int i = 1; i <= N; i++) {
-			short input[5001];
-			int idx = 0;
-			char temp;
-
-			while (1) {
-				scanf("%d%c", &input[idx++], &temp);
-				if (temp == '\n')
-					break;
-			}
-
-			for (int j = 0; j < idx; j++) {
-				int to = input[j];
-
-				graph[i + OUT].push_back(to + IN);
-				graph[to + IN].push_back(i + OUT);
-
-				capacity[i + OUT][to + IN] = 1;
-				capacity[to + OUT][i + IN] = 1;
-			}
-		}
-
-
-		cout << "Case " << tt << ":" << endl;
-
-		int start = 5001;
-		int des = 2;
-
-		int numOfPath = maxFlow(start, des);
-
-		if (K == g_count) {
-			for (int i = 0; i < g_count; i++) {
-				cout << 1 << " ";
-
-				for (int j = path[i].size() - 1; j >= 0; j--) {
-					cout << path[i][j] << " ";
-				}
-				cout << endl;
-			}
-		}
-		else {
-			cout << "Impossible" << endl;
-		}
-		cout << endl;
-
-		tt++;
 	}
-	return 0;
+
+	int count = 0;
+
+	for (int i = 1; i <= N; i++) {
+		fill(c, c + MAX, false);
+		if (dfs(i))
+			count++;
+	}
+
+	cout << count << endl;
 }

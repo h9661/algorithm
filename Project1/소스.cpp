790f12af5ffb414b755cb0e5878baea16b73527b
h9661 <guscksdn123@gmail.com>
2021-09-19 14:11:58 +0900

    BackJoon 10830 분할정복을 이용한 행렬 제곱

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -9,24 +9,69 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-int gcd(int a, int b) {
-	while (b != 0) {
-		int r = a % b;
-		a = b;
-		b = r;
+//10830
+
+const int MAX = 5 + 1;
+const int mod = 1000;
+ll N, B;
+
+vector<vector<ull>> multiple(vector<vector<ull>>& a, vector<vector<ull>>& b) {
+	vector<vector<ull>> retval(MAX, vector<ull>(MAX, 0));
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			for (int k = 1; k <= N; k++)
+				retval[i][j] += a[i][k] * b[k][j];
+			retval[i][j] %= mod;
+		}
 	}
 
-	return a;
+	return retval;
 }
 
+
 int main() {
-	int tc;
-	cin >> tc;
+	cin >> N >> B;
 
-	while (tc--) {
-		int A, B;
-		cin >> A >> B;
+	vector<vector<ull>> E(MAX, vector<ull>(MAX, 0));
 
-		cout << (A * B) / gcd(A, B) << endl;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (i == j)
+				E[i][j] = 1;
+		}
+	}
+
+	vector<vector<ull>> A(MAX, vector<ull>(MAX, 0));
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
+			cin >> A[i][j];
+	}
+
+	while (B > 0) {
+		if (B % 2 == 1) {
+			E = multiple(E, A);
+		}
+
+		A = multiple(A, A);
+		B /= 2;
+	}
+
+	if (B == 1) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				cout << E[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+	else {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				cout << E[i][j] % 1000 << " ";
+			}
+			cout << endl;
+		}
 	}
 }

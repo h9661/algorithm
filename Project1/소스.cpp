a10260e847c80a7ebb73c4e1d710b3fc2674af59
h9661 <guscksdn123@gmail.com>
2021-09-27 11:36:24 +0900

    BackJoon 11718 문자열 구현

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -9,49 +9,15 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 200 + 1;
-vector<int> graph[MAX];
-bool capacity[MAX];
-int parent[MAX];
-int N, M;
-
-bool dfs(int x) {
-	for (int i = 0; i < graph[x].size(); i++) {
-		int y = graph[x][i];
-
-		if (capacity[y])
-			continue;
-		capacity[y] = true;
-
-		if (parent[y] == 0 || dfs(parent[y])) {
-			parent[y] = x;
-			return true;
-		}
-	}
-
-	return false;
-}
-
 int main() {
-	cin >> N >> M;
+	string s;
 
-	const int ROOM = 100;
+	while (getline(cin, s)) {
+		if (s == "")
+			break;
 
-	for (int i = 0; i < M; i++) {
-		int person, notebook;
-		cin >> person >> notebook;
-
-		graph[person].push_back(notebook + ROOM);
+		cout << s << endl;
 	}
-
-	int count = 0;
-
-	for (int i = 1; i <= N; i++) {
-		fill(capacity, capacity + MAX, false);
-
-		if (dfs(i))
-			count++;
-	}
-
-	cout << count << endl;
+	
+	return 0;
 }

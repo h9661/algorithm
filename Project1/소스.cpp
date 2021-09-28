3c9998daf05fb4726778ff881e759303780b2607
h9661 <guscksdn123@gmail.com>
2021-09-28 15:44:35 +0900

    BackJoon 1786 kmp법

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -9,7 +9,58 @@
 #define pll pair<ll, ll>
 using namespace std;
 
+string P, T;
+int result = 0;
+vector<int> result_vec;
+
+vector<int> makeTable(const string& pattern) {
+	int patternSize = pattern.size();
+	vector<int> table(patternSize, 0);
+
+	int j = 0;
+	for (int i = 1; i < patternSize; i++) {
+		while (j > 0 && pattern[i] != pattern[j])
+			j = table[j - 1];
+
+		if (pattern[i] == pattern[j])
+			table[i] = ++j;
+	}
+
+	return table;
+}
+
+void kmp(const string& text, const string& pattern) {
+	vector<int> table = makeTable(pattern);
+
+	int textSize = text.size();
+	int patternSize = pattern.size();
+
+	int j = 0;
+	for (int i = 0; i < textSize; i++) {
+		while (j > 0 && text[i] != pattern[j])
+			j = table[j - 1];
+
+		if (text[i] == pattern[j]) {
+			if (j == patternSize - 1) {
+				result_vec.push_back(i - patternSize + 2);
+				j = table[j];
+				result++;
+			}
+			else
+				j++;
+		}
+	}
+}
+
 int main() {
-	string s; cin >> s;
-	cout << s.size() << endl;
+	getline(cin, T);
+	getline(cin, P);
+
+	kmp(T, P);
+
+	cout << result << endl;
+	for (int i = 0; i < result_vec.size(); i++)
+		cout << result_vec[i] << " ";
+
+	return 0;
 }

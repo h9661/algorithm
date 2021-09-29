59ca1d047b088da24b5ce822b084e69dc9f1911c
h9661 <guscksdn123@gmail.com>
2021-09-29 09:33:25 +0900

    BackJoon 1991 트리 구현과 순회

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -9,25 +9,55 @@
 #define pll pair<ll, ll>
 using namespace std;
 
+const int MAX = 25 + 1;
+vector<pair<int, bool>> tree[MAX];
+
+void preOrder(int node) {
+	cout << (char)(node + 'A');
+	for (int i = 0; i < tree[node].size(); i++)
+		preOrder(tree[node][i].first);
+}
+
+void inOrder(int node) {
+	if (tree[node].size() && tree[node][0].second == true)
+		inOrder(tree[node][0].first);
+
+	cout << (char)(node + 'A');
+
+	if (tree[node].size() && tree[node][0].second == false)
+		inOrder(tree[node][0].first);
+	else if (tree[node].size() == 2)
+		inOrder(tree[node][1].first);
+}
+
+void postOrder(int node) {
+	for (int i = 0; i < tree[node].size(); i++)
+		postOrder(tree[node][i].first);
+
+	cout << (char)(node + 'A');
+}
+
 int main() {
-	bool flag = true;
-	int count = 0;
+	int N;
+	cin >> N;
 
-	for (int i = 0; i < 8; i++) {
-		string s;
-		getline(cin, s);
+	for (int i = 0; i < N; i++) {
+		char a, b, c;
+		cin >> a >> b >> c;
 
-		for (int j = 0; j < 8; j++) {
-			if (flag) {
-				if (s[j] == 'F')
-					count++;
-			}
+		if (b != '.')
+			tree[a - 'A'].push_back({ b - 'A', true });
 
-			flag = !flag;
-		}
-
-		flag = !flag;
+		if (c != '.')
+			tree[a - 'A'].push_back({ c - 'A' , false });
 	}
 
-	cout << count << endl;
+	preOrder(0);
+	cout << endl;
+	inOrder(0);
+	cout << endl;
+	postOrder(0);
+	cout << endl;
+
+	return 0;
 }

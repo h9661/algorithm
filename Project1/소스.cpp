5c28c1e80063679ff81e72ad5051ee6fa8fa9154
h9661 <guscksdn123@gmail.com>
2021-09-30 21:07:58 +0900

    BackJoon 2163 수학

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -10,58 +10,8 @@
 using namespace std;
 
 int main() {
-	int tc;
-	cin >> tc;
+	int N, M;
+	cin >> N >> M;
 
-	while (tc--) {
-		int N, M;
-		cin >> N >> M;
-
-		vector<int> times(N + 1, 0);
-		vector<int> costs(N + 1, 0);
-		vector<int> indeg(N + 1, 0);
-		vector<vector<int>> graph(N + 1);
-
-		for (int i = 1; i <= N; i++)
-			cin >> times[i];
-
-		for (int i = 0; i < M; i++) {
-			int from, to;
-			cin >> from >> to;
-
-			graph[from].push_back(to);
-			indeg[to]++;
-		}
-
-		int W;
-		cin >> W;
-
-		queue<int> q;
-
-		for (int i = 1; i <= N; i++) {
-			if (indeg[i] == 0) {
-				q.push(i);
-				costs[i] = times[i];
-			}
-		}
-
-		while (!q.empty()) {
-			int curNode = q.front();
-			int curCost = costs[curNode];
-			q.pop();
-
-			for (int i = 0; i < graph[curNode].size(); i++) {
-				int nextNode = graph[curNode][i];
-				int nextCost = times[nextNode] + curCost;
-
-				if (--indeg[nextNode] == 0)
-					q.push(nextNode);
-
-				if (costs[nextNode] < nextCost)
-					costs[nextNode] = nextCost;
-			}
-		}
-
-		cout << costs[W] << endl;
-	}
+	cout << N * M - 1 << endl;
 }

7db73554b28b233a1b608a89df6f2cfa50c4630d
h9661 <guscksdn123@gmail.com>
2021-10-03 16:06:19 +0900

    BackJoon 3190 시뮬레이션, 구현

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -9,43 +9,91 @@
 #define pll pair<ll, ll>
 using namespace std;
 
+const int MAX = 100 + 1;
+int field[MAX][MAX];
+
 int main() {
+	fastio;
 	int N;
 	cin >> N;
+	int K;
+	cin >> K;
+	for (int i = 0; i < K; i++) {
+		int y, x;
+		cin >> y >> x;
 
-	stack<int> arr;
-	for (int i = 0; i < N; i++) {
-		int temp;
-		cin >> temp;
-
-		arr.push(temp);
+		field[y - 1][x - 1] = 1;
 	}
 
-	stack<int> stk;
+	vector<pair<int, int>> snakeBody;
+	pair<int, int> snakeHead = { 0, 0 };
+	pair<int, int> MovingDir[4] = {
+		{0, 1},
+		{1, 0},
+		{0, -1},
+		{-1, 0},
+	};
 
-	vector<int> ans;
+	int L;
+	cin >> L;
+	queue<pair<int, char>> changeDir;
+	for (int i = 0; i < L; i++) {
+		int time;
+		char Dir;
+		cin >> time >> Dir;
 
-	while(arr.size() != 0){
-		int num = arr.top();
-
-		if (stk.size() == 0) {
-			stk.push(num);
-			arr.pop();
-			ans.push_back(-1);
-			continue;
-		}
-
-		if (num < stk.top()) {
-			ans.push_back(stk.top());
-			stk.push(num);
-			arr.pop();
-		}
-		else
-			stk.pop();
+		changeDir.push({ time, Dir });
 	}
 
-	for (int i = ans.size() - 1; i >= 0; i--)
-		cout << ans[i] << " ";
+	int time = 0;
+	int direction = 0;
 
-	return 0;
+	while (1) {
+		int tailTempY = snakeHead.first;
+		int tailTempX = snakeHead.second;
+		snakeHead.first += MovingDir[direction].first;
+		snakeHead.second += MovingDir[direction].second;
+		time += 1;
+
+		if (snakeHead.first < 0 || snakeHead.first > N - 1 || snakeHead.second < 0 || snakeHead.second > N - 1)
+			break;
+
+		for (int i = 0; i < snakeBody.size(); i++) {
+			if (snakeHead.first == snakeBody[i].first && snakeHead.second == snakeBody[i].second) {
+				goto EXIT;
+			}
+		}
+
+		if (field[snakeHead.first][snakeHead.second] == 1) {
+			snakeBody.insert(snakeBody.begin(), { tailTempY, tailTempX });
+			field[snakeHead.first][snakeHead.second] = 0;
+		}
+		else if (field[snakeHead.first][snakeHead.second] == 0) {
+			if (snakeBody.size() >= 1) {
+				snakeBody.pop_back();
+				snakeBody.insert(snakeBody.begin(), { tailTempY, tailTempX });
+			}
+		}
+
+		if (changeDir.size() > 0) {
+			if (time == changeDir.front().first) {
+				if (changeDir.front().second == 'D') {
+					direction += 1;
+					if (direction > 3)
+						direction = 0;
+				}
+				else {
+					direction -= 1;
+					if (direction < 0)
+						direction = 3;
+				}
+
+				changeDir.pop();
+			}
+		}
+	}
+
+EXIT:
+
+	cout << time << endl;
 }

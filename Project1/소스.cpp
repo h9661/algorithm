a3378d2bd6dac368c43655ed28f64e2542f4dbbe
h9661 <guscksdn123@gmail.com>
2021-10-04 19:28:47 +0900

    BackJoon 14888 백트래킹

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -9,20 +9,77 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-int main() {
-	string S;
-	cin >> S;
+int N;
+int maxValue = -987654321;
+int minValue = 987654321;
+vector<int> opCount(4, 0);
+vector<int> arr(101, 0);
+int op[101];
 
-	vector<string> subStr;
+void dfs(int x) {
+	if (x == N - 1) {
+		int temp = 0;
 
-	for (int i = 0; i < S.size(); i++) {
-		string temp = S.substr(i);
+		for (int i = 0; i < N - 1; i++) {
+			if		(op[i] == 0) {
+				if (i == 0)
+					temp = arr[i] + arr[i + 1];
+				else
+					temp = temp + arr[i + 1];
+			}
+			else if (op[i] == 1) {
+				if (i == 0)
+					temp = arr[i] - arr[i + 1];
+				else
+					temp = temp - arr[i + 1];
+			}
+			else if (op[i] == 2) {
+				if (i == 0)
+					temp = arr[i] * arr[i + 1];
+				else
+					temp = temp * arr[i + 1];
+			}
+			else if (op[i] == 3) {
+				if (i == 0)
+					temp = arr[i] / arr[i + 1];
+				else
+					temp = temp / arr[i + 1];
+			}
+		}
 
-		subStr.push_back(temp);
+		maxValue = max(maxValue, temp);
+		minValue = min(minValue, temp);
+
+		return;
 	}
 
-	sort(subStr.begin(), subStr.end());
+	for (int i = 0; i < 4; i++) {
+		if (opCount[i] > 0) {
+			opCount[i] -= 1;
 
-	for (int i = 0; i < subStr.size(); i++)
-		cout << subStr[i] << endl;
+			op[x] = i;
+			dfs(x + 1);
+
+			opCount[i] += 1;
+		}
+	}
+}
+
+
+int main() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < 4; i++)
+		cin >> opCount[i];
+
+
+	dfs(0);
+
+	cout << maxValue << endl;
+	cout << minValue << endl;
+
+	return 0;
 }

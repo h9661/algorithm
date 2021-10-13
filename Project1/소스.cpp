e338430c3249993c73f9b82780f6e143977f2e74
h9661 <guscksdn123@gmail.com>
2021-10-13 12:45:11 +0900

    BackJoon 2660 플로이드 와샬

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -12,27 +12,69 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-int dp[1001];
+const int MAX = 50 + 2;
+int graph[MAX][MAX];
+const int INF = 2e5;
 
 int main() {
+	fill(&graph[0][0], &graph[MAX - 1][MAX], INF);
 	int N;
 	cin >> N;
 
-	dp[1] = 1;
-	dp[2] = 0;
-	dp[3] = 1;
-
-	for (int i = 4; i <= 1000; i++) {
-		if (dp[i - 1] == 0 || dp[i - 3] == 0)
-			dp[i] = 1;
-		else
-			dp[i] = 0;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (i == j)
+				graph[i][j] = 0;
+		}
 	}
 
-	if (dp[N] == 1)
-		cout << "SK" << endl;
-	else
-		cout << "CY" << endl;
+
+	while (1) {
+		int u, v;
+		cin >> u >> v;
+
+		if (u == -1 and v == -1)
+			break;
+
+		graph[u][v] = 1;
+		graph[v][u] = 1;
+	}
+
+	for (int k = 1; k <= N; k++) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (graph[i][j] > graph[i][k] + graph[k][j])
+					graph[i][j] = graph[i][k] + graph[k][j];
+			}
+		}
+	}
+
+	vector<int> store(N + 1, 0);
+
+	for (int i = 1; i <= N; i++) {
+		int maximum = 0;
+
+		for (int j = 1; j <= N; j++) {
+			if (graph[i][j] > maximum)
+				maximum = graph[i][j];
+		}
+
+		store[i] = maximum;
+	}
+
+	int min_num = *min_element(store.begin() + 1, store.end());
+	int count = 0;
+	vector<int> people;
+	for (int i = 1; i <= N; i++) {
+		if (min_num == store[i]) {
+			count++;
+			people.push_back(i);
+		}
+	}
+
+	cout << min_num << " " << count << endl;
+	for (auto i : people)
+		cout << i << " ";
 
 	return 0;
 }

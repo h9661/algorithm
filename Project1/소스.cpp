f4eaaa3381063fa6fa48056f2d14acc2cdb2bf29
h9661 <guscksdn123@gmail.com>
2021-10-16 15:53:58 +0900

    BackJoon 9659 수학

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -12,26 +12,11 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-int dp[1001];
-
 int main() {
-	fastio;
-	int N;
+	ll N;
 	cin >> N;
 
-	dp[1] = 1;
-	dp[2] = 0;
-	dp[3] = 1;
-	dp[4] = 1;
-
-	for (int i = 5; i <= 1000; i++) {
-		if (!dp[i - 1] || !dp[i - 3] || !dp[i - 4])
-			dp[i] = 1;
-		else
-			dp[i] = 0;
-	}
-
-	if (dp[N] == 1)
+	if (N % 2 == 1)
 		cout << "SK" << endl;
 	else
 		cout << "CY" << endl;

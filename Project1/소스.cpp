82bbe22acd8603cf2d5d4bdad5ea628589f26c51
h9661 <guscksdn123@gmail.com>
2021-10-31 18:33:55 +0900

    BackJoon 1547 구현

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -13,46 +13,27 @@
 using namespace std;
 
 int main() {
-    string eight;
-    cin >> eight;
-    for (int i = 0; i < eight.size(); i++)
-    {
-        if (eight[i] == '0')
-        {
-            if (i == 0)
-                cout << '0';
-            else
-                cout << "000";
-        }
-        else if (eight[i] == '1')
-        {
-            if (i == 0)
-                cout << "1";
-            else
-                cout << "001";
-        }
-        else if (eight[i] == '2')
-        {
-            if (i == 0)
-                cout << "10";
-            else
-                cout << "010";
+	int M;
+	cin >> M;
 
-        }
-        else if (eight[i] == '3')
-        {
-            if (i == 0)
-                cout << "11";
-            else
-                cout << "011";
-        }
-        else if (eight[i] == '4')
-            cout << "100";
-        else if (eight[i] == '5')
-            cout << "101";
-        else if (eight[i] == '6')
-            cout << "110";
-        else if (eight[i] == '7')
-            cout << "111";
-    }
+	vector<int> arr(4, 0);
+	arr[1] = 1;
+
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+
+		int temp = arr[b];
+		arr[b] = arr[a];
+		arr[a] = temp;
+	}
+
+	for (int i = 1; i <= 3; i++) {
+		if (arr[i] == 1) {
+			cout << i << endl;
+			return 0;
+		}
+	}
+
+	cout << -1 << endl;
 }

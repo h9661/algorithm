a7616b7cc13c4b532ab46293d68d639616ecadbc
h9661 <guscksdn123@gmail.com>
2021-11-09 19:17:07 +0900

    BackJoon 17070 그래프탐색 dfs

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -3,6 +3,7 @@
 #pragma warning (disable : 6385)
 #pragma warning (disable : 6386)
 #pragma warning (disable : 6200)
+#pragma warning (disable : 26812)
 #define ull unsigned long long
 #define ll long long int
 #define ui unsigned int
@@ -12,64 +13,91 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 50;
+const int MAX = 16 + 1;
 int arr[MAX][MAX];
-int N, M;
-vector<pii> homePosArr;
-vector<pair<pii, bool>> chPosArr;
-int ans = 2e9;
+int N;
+int ans = 0;
 
-int FindDistance(pii x, pii y) {
-	return abs(x.first - y.first) + abs(x.second - y.second);
-}
+typedef enum Type {
+	TYPE1 = 0,
+	TYPE2 = 1,
+	TYPE3 = 2
+}PIPE_TYPE;
 
-void dfs(int idx, int count) {
-	if (count == M) {
-		int sum = 0;
-
-		for (int i = 0; i < homePosArr.size(); i++) {
-			int minDist = 2e9;
-
-			for (int j = 0; j < chPosArr.size(); j++) {
-				if(chPosArr[j].second == true)
-					minDist = min(minDist, FindDistance(homePosArr[i], chPosArr[j].first));
+void dfs(pii start, pii end, PIPE_TYPE type) {
+	if (end.first == N && end.second == N) {
+		if (type == TYPE2) {
+			for (int i = start.first; i <= end.first; i++) {
+				for (int j = start.second; j <= end.second; j++) {
+					if (arr[i][j] == 1)
+						return;
+				}
 			}
-
-			sum += minDist;
+		}
+		else if (type == TYPE1) {
+			if (arr[start.first][start.second] == 1 || arr[end.first][end.second] == 1)
+				return;
+		}
+		else {
+			if (arr[start.first][start.second] == 1 || arr[end.first][end.second] == 1)
+				return;
 		}
 
-		ans = min(sum, ans);
-
+		ans++;
+		return;
 	}
-	else {
-		for (int i = idx; i < chPosArr.size(); i++) {
-			if (chPosArr[i].second == false) {
-				chPosArr[i].second = true;
 
-				dfs(i, count + 1);
+	if (type == TYPE1) {
+		if (end.first > N || end.second > N)
+			return;
 
-				chPosArr[i].second = false;
+		if (arr[start.first][start.second] == 1 || arr[end.first][end.second] == 1)
+			return;
+		else {
+			dfs({ start.first, start.second + 1 }, { end.first, end.second + 1 }, TYPE1);
+			dfs({ start.first, start.second + 1 }, { end.first + 1, end.second + 1 }, TYPE2);
+		}
+	}
+
+	if (type == TYPE2) {
+		if (end.first > N || end.second > N)
+			return;
+
+		for (int i = start.first; i <= end.first; i++) {
+			for (int j = start.second; j <= end.second; j++) {
+				if (arr[i][j] == 1)
+					return;
 			}
+		}
+
+		dfs({ start.first + 1, start.second + 1 }, { end.first, end.second + 1 }, TYPE1);
+		dfs({ start.first + 1, start.second + 1 }, { end.first + 1, end.second + 1 }, TYPE2);
+		dfs({ start.first + 1, start.second + 1 }, { end.first + 1, end.second }, TYPE3);
+	}
+
+	if (type == TYPE3) {
+		if (end.first > N || end.second > N)
+			return;
+
+		if (arr[start.first][start.second] == 1 || arr[end.first][end.second] == 1)
+			return;
+		else {
+			dfs({start.first + 1, start.second}, {end.first + 1, end.second + 1}, TYPE2);
+			dfs({ start.first + 1, start.second }, { end.first + 1, end.second }, TYPE3);
 		}
 	}
 }
 
 int main() {
 	fastio;
-	cin >> N >> M;
+	cin >> N;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
 			cin >> arr[i][j];
-
-			if (arr[i][j] == 1)
-				homePosArr.push_back(make_pair(i, j));
-			else if (arr[i][j] == 2)
-				chPosArr.push_back({ make_pair(i, j), false });
-		}
 	}
 
-	dfs(0, 0);
+	dfs({ 1, 1 }, { 1, 2 }, TYPE1);
 
 	cout << ans << endl;
 }

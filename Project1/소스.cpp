4b3c144f5c3f744aa7acf4551d48c73685088327
h9661 <guscksdn123@gmail.com>
2021-11-27 18:15:13 +0900

    BackJoon 9184 DP + 재귀

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -13,49 +13,48 @@
 #define pll pair<ll, ll>
 using namespace std;
 
+int dp[51][51][51];
+
+ll w(int a, int b, int c) {
+	if (a <= 0 || b <= 0 || c <= 0)
+		return 1;
+	else if (a > 20 || b > 20 || c > 20) {
+		if (!dp[20][20][20])
+			dp[20][20][20] = w(20, 20, 20);
+
+		return dp[20][20][20];
+	}
+	else if (a < b and b < c) {
+		if (!dp[a][b][c - 1])
+			dp[a][b][c - 1] = w(a, b, c - 1);
+		if (!dp[a][b - 1][c - 1])
+			dp[a][b - 1][c - 1] = w(a, b - 1, c - 1);
+		if (!dp[a][b - 1][c])
+			dp[a][b - 1][c] = w(a, b - 1, c);
+
+		return dp[a][b][c - 1] + dp[a][b - 1][c - 1] - dp[a][b - 1][c];
+	}
+	else {
+		if (!dp[a - 1][b][c])
+			dp[a - 1][b][c] = w(a - 1, b, c);
+		if (!dp[a - 1][b - 1][c])
+			dp[a - 1][b - 1][c] = w(a - 1, b - 1, c);
+		if (!dp[a - 1][b - 1][c - 1])
+			dp[a - 1][b - 1][c - 1] = w(a - 1, b - 1, c - 1);
+
+		return dp[a - 1][b][c] + dp[a - 1][b - 1][c] + w(a - 1, b, c - 1) - dp[a - 1][b - 1][c - 1];
+	}
+}
+
 int main() {
-	fastio;
-	queue<int> q;
+	while (1) {
+		int a, b, c;
+		cin >> a >> b >> c;
 
-	int N;
-	cin >> N;
+		if (a == -1 and b == -1 and c == -1)
+			break;
 
-	while (N--) {
-		string s;
-		cin >> s;
-
-		if (s == "push") {
-			int k;
-			cin >> k;
-			q.push(k);
-		}
-		if (s == "pop") {
-			if (q.empty())
-				cout << -1 << endl;
-			else {
-				cout << q.front() << endl;
-				q.pop();
-			}
-		}
-		if (s == "size")
-			cout << q.size() << endl;
-		if (s == "empty") {
-			if (q.empty())
-				cout << 1 << endl;
-			else
-				cout << 0 << endl;
-		}
-		if (s == "front") {
-			if (q.empty())
-				cout << -1 << endl;
-			else
-				cout << q.front() << endl;
-		}
-		if (s == "back") {
-			if (q.empty())
-				cout << -1 << endl;
-			else
-				cout << q.back() << endl;
-		}
+		printf("w(%d, %d, %d) = ", a, b, c);
+		cout << w(a, b, c) << endl;
 	}
 }

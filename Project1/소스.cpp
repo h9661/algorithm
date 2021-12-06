ce1c8228897313f174bd044968e2b929a2aeca3f
h9661 <guscksdn123@gmail.com>
2021-12-06 16:58:47 +0900

    BackJoon 14241 그리디 + 정렬

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -13,59 +13,25 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-int k;
-vector<char> arr;
-char selects[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-bool check[10];
-string temp;
-vector<string> result;
-
-void backtracking(int count) {
-	if (count == k + 1) {
-		for (int i = 0; i < k; i++) {
-			if (arr[i] == '<') {
-				if (temp[i] > temp[i + 1])
-					return;
-			}
-			if (arr[i] == '>') {
-				if (temp[i] < temp[i + 1])
-					return;
-			}
-		}
-		result.push_back(temp);
-	}
-	else {
-		for (int i = 0; i < 10; i++) {
-			if (check[i] == false) {
-				check[i] = true;
-				
-				temp.push_back(i + '0');
-				backtracking(count + 1);
-				temp.pop_back();
-
-				check[i] = false;
-			}
-		}
-	}
-}
-
 int main() {
-	fastio;
-	cin >> k;
+	int N;
+	cin >> N;
 
+	int ans = 0;
 
-	for (int i = 0; i < k; i++) {
-		char temp;
-		cin >> temp;
-		arr.push_back(temp);
+	vector<int> arr(N, 0);
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	sort(arr.begin(), arr.end());
+
+	int temp = arr[0] + arr[1];
+	ans += arr[0] * arr[1];
+
+	for (int i = 2; i < N; i++) {
+		ans += temp * arr[i];
+		temp = temp + arr[i];
 	}
 
-	backtracking(0);
-
-	sort(result.begin(), result.end());
-
-	cout << result.at(result.size() - 1) << endl;
-	cout << result.at(0) << endl;
-
-	return 0;
+	cout << ans << endl;
 }

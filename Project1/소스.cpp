41c4d35e9f330251720559f0c48fc3ad601532b0
h9661 <guscksdn123@gmail.com>
2021-12-09 22:23:53 +0900

    BackJoon 5086 정수론

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -13,44 +13,21 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-int N;
-vector<int> arr;
-vector<bool> check;
-vector<int> temp;
-int ans = -1;
-
-void backtracking(int count) {
-	if (count == N) {
-		int sum = 0;
-		for (int i = 0; i < N - 1; i++)
-			sum += abs(temp[i] - temp[i + 1]);
-
-		ans = max(ans, sum);
-	}
-	else {
-		for (int i = 0; i < N; i++) {
-			if (check[i] == false) {
-				check[i] = true;
-
-				temp.push_back(arr[i]);
-				backtracking(count + 1);
-				temp.pop_back();
-
-				check[i] = false;
-			}
-		}
-	}
-}
-
 int main() {
-	fastio;
-	cin >> N;
-	arr.resize(N, 0);
-	check.resize(N, 0);
-	for (int i = 0; i < N; i++)
-		cin >> arr[i];
+	while (1) {
+		int a, b;
+		cin >> a >> b;
 
-	backtracking(0);
-	
-	cout << ans << endl;
+		if (a == 0 and b == 0)
+			break;
+
+		if (b % a == 0 && a < b)
+			cout << "factor" << endl;
+		else if (a % b == 0 && a > b)
+			cout << "multiple" << endl;
+		else
+			cout << "neither" << endl;
+	}
+
+	return 0;
 }

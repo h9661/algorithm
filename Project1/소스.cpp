c35f1e5fa28a530b3f64990344b9add4a651c448
h9661 <guscksdn123@gmail.com>
2022-01-05 13:34:32 +0900

    BackJoon 2583 그래프 탐색

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -13,49 +13,71 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-int arr[1000001];
+const int MAX = 100;
+int graph[MAX][MAX];
+int Y[4] = { -1, 1, 0, 0 };
+int X[4] = { 0, 0, -1, 1 };
+int N, M, K;
+int ans = 0;
+vector<int> area;
 
-void eratos() {
-	for (int i = 2; i <= 1000000; i++)
-		arr[i] = i;
+void bfs(int y, int x) {
+	int count = 0;
+	queue<pii> q;
+	q.push({ y, x });
+	graph[y][x] = 1;
+	count++;
 
-	for (int i = 2; i <= 1000000; i++) {
-		if (arr[i] == 0)
-			continue;
+	while (!q.empty()) {
+		int cy = q.front().first;
+		int cx = q.front().second;
+		q.pop();
 
-		for (int j = i + i; j <= 1000000; j += i)
-			arr[j] = 0;
+		for (int i = 0; i < 4; i++) {
+			int ny = cy + Y[i];
+			int nx = cx + X[i];
+
+			if (ny < M && ny >= 0 && nx < N && nx >= 0) {
+				if (graph[ny][nx] == 0) {
+					q.push({ ny, nx });
+					graph[ny][nx] = 1;
+					count++;
+				}
+			}
+		}
 	}
+
+	area.push_back(count);
 }
 
 int main() {
-	eratos();
 	fastio;
 
-	while (1) {
-		int n;
-		cin >> n;
 
-		if (n == 0)
-			break;
+	cin >> N >> M >> K;
 
-		int left = 3;
-		int right = n - 3;
+	for (int i = 0; i < K; i++) {
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
 
-		while (left <= right) {
-			if (arr[left] != 0 && arr[right] != 0)
-				if (left + right == n)
-					break;
-
-			if (left + right < n)
-				left += 2;
-			else
-				right -= 2;
+		for (int y = x1; y < x2; y++) {
+			for (int x = y1; x < y2; x++) {
+				graph[y][x] = 1;
+			}
 		}
-
-		if (left > right)
-			cout << "Goldbach's conjecture is wrong." << endl;
-		else 
-			cout << n << " = " << left << " + " << right << '\n';
 	}
+
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			if (graph[i][j] == 0) {
+				bfs(i, j);
+				ans++;
+			}
+		}
+	}
+	sort(area.begin(), area.end());
+	cout << ans << endl;
+	for (auto i : area)
+		cout << i << " ";
+	return 0;
 }

69bb81044d945545ad326cea158e7a4a3b79c8af
h9661 <guscksdn123@gmail.com>
2022-01-10 14:28:16 +0900

    BackJoon 2636 넓이 우선 탐색, 시뮬레이션

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,9 +1,4 @@
 #include <bits/stdc++.h>
-#pragma warning (disable : 4996)
-#pragma warning (disable : 6385)
-#pragma warning (disable : 6386)
-#pragma warning (disable : 6200)
-#pragma warning (disable : 26812)
 #define ull unsigned long long
 #define ll long long int
 #define ui unsigned int
@@ -13,13 +8,79 @@
 #define pll pair<ll, ll>
 using namespace std;
 
-int N;
-string ans;
+const int MAX = 101;
+int N, M;
+int arr[MAX][MAX];
+int Y[4] = { 1, -1, 0, 0 };
+int X[4] = { 0, 0, -1, 1 };
 
-bool IsGoodSequence(string str) {
-	for (int i = 1; i <= str.size() / 2; i++) {
-		for (int idx = 0; idx < str.size() - i; idx++) {
-			if (str.substr(idx, i) == str.substr(idx + i, i))
+
+void Air() {
+	queue<pii> q;
+	q.push({ 1, 1 });
+	arr[1][1] = 3;
+
+	while (!q.empty()) {
+		int cy = q.front().first;
+		int cx = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int ny = Y[i] + cy;
+			int nx = X[i] + cx;
+
+			if (ny >= 1 && ny <= N && nx >= 1 && nx <= M) {
+				if (arr[ny][nx] == 0) {
+					arr[ny][nx] = 3;
+					q.push({ ny, nx });
+				}
+			}
+		}
+	}
+}
+
+void Melt() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			if (arr[i][j] == 1) {
+				for (int k = 0; k < 4; k++) {
+					int ny = i + Y[k];
+					int nx = j + X[k];
+
+					if (arr[ny][nx] == 3)
+						arr[i][j] = 4;
+				}
+			}
+		}
+	}
+}
+
+void Set() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			if (arr[i][j] == 3)
+				arr[i][j] = 0;
+			if (arr[i][j] == 4)
+				arr[i][j] = 0;
+		}
+	}
+}
+
+int Count() {
+	int count = 0;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++)
+			if (arr[i][j] == 1)
+				count++;
+	}
+
+	return count;
+}
+
+bool Check() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			if (arr[i][j] == 1)
 				return false;
 		}
 	}
@@ -27,26 +88,31 @@ bool IsGoodSequence(string str) {
 	return true;
 }
 
-void backtracking(int count) {
-	if (count == N) {
-		cout << ans << endl;
-		exit(0);
-	}
-	else {
-		for (int i = 1; i <= 3; i++) {
-			if (IsGoodSequence(ans + to_string(i))) {
-				ans += to_string(i);
-
-				backtracking(count + 1);
-				
-				ans.pop_back();
-			}
-		}
-	}
-}
-
 int main() {
 	fastio;
-	cin >> N;
-	backtracking(0);
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++)
+			cin >> arr[i][j];
+	}
+	int ans = 0;
+	int count = 0;
+	while (1) {
+		bool flag = Check();
+
+		if (flag == true)
+			break;
+
+		int temp = Count();
+		if (temp != 0)
+			count = temp;
+		Air();
+		Melt();
+		Set();
+		ans++;
+	}
+
+	cout << ans << endl;
+	cout << count << endl;
+
 }

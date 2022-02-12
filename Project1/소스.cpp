272a767c494dc831fed9c85139437896e1f887da
h9661 <guscksdn123@gmail.com>
2022-02-12 09:58:20 +0900

    BackJoon 16234 시뮬레이션, 너비우선탐색

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,73 +1,121 @@
 #include <bits/stdc++.h>
-#define ull unsigned long long
-#define ll long long int
-#define ui unsigned int
-#define endl '\n'
-#define fastio (cin.tie(NULL), cout.sync_with_stdio(false), cout.tie(NULL))
-#define pii pair<int, int>
-#define pll pair<ll, ll>
 using namespace std;
 
-const int MAX = 5000 + 1;
-ll arr[MAX];
+const int MAX = 51;
 
-int main() {
-	int N;
-	cin >> N;
+int graph[MAX][MAX];
+int check[MAX][MAX];
+map<int, pair<int, int>> mp;
+int Y[4] = { -1, 1, 0, 0 };
+int X[4] = { 0, 0, -1, 1 };
+int N, L, R;
+int g_number = 0;
 
-	for (int i = 0; i < N; i++)
-		cin >> arr[i];
+void Input() {
+	cin >> N >> L >> R;
 
-	sort(arr, arr + N);
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
+			cin >> graph[i][j];
+	}
+}
 
-	int left = 0;
-	int right = N - 1;
+void NumberingMap() {
+	int number = 1;
 
-	ll minimum = 4e9;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (check[i][j] == 0) {
+				queue<pair<int, int>> q;
 
-	vector<ll> store;
-	ll temp1 = arr[0];
-	ll temp2 = arr[1];
-	ll temp3 = arr[2];
+				int sum = 0;
+				int count = 0;
 
-	for (int i = 0; i < N; i++) {
-		left = 0;
-		right = N - 1;
+				q.push({ i, j });
+				check[i][j] = number;
+				count += 1;
+				sum += graph[i][j];
 
-		while (left < right) {
-			ll sum = arr[left] + arr[right] + arr[i];
 
-			if (abs(sum) < minimum) {
-				if (i == left || i == right) {
-					if (sum < 0)
-						left += 1;
-					else
-						right -= 1;
-					continue;
+				while (!q.empty()) {
+					int cy = q.front().first;
+					int cx = q.front().second;
+					q.pop();
+
+					for (int i = 0; i < 4; i++) {
+						int ny = cy + Y[i];
+						int nx = cx + X[i];
+
+						if (ny >= 1 && ny <= N && nx >= 1 && nx <= N && check[ny][nx] == 0) {
+							if (abs(graph[ny][nx] - graph[cy][cx]) >= L && abs(graph[ny][nx] - graph[cy][cx]) <= R) {
+								q.push({ ny, nx });
+								count++;
+								check[ny][nx] = number;
+								sum += graph[ny][nx];
+							}
+						}
+					}
 				}
 
-				minimum = abs(sum);
-				temp1 = arr[left];
-				temp2 = arr[right];
-				temp3 = arr[i];
+				mp[number] = { sum, count };
+				number++;
 			}
-
-			if (sum < 0)
-				left += 1;
-			else
-				right -= 1;
-
 		}
 	}
 
-	store.push_back(temp1);
-	store.push_back(temp2);
-	store.push_back(temp3);
+	g_number = number;
+}
 
-	sort(store.begin(), store.end());
+void TransferPeople() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			graph[i][j] = mp[check[i][j]].first / mp[check[i][j]].second;
+		}
+	}
+}
 
-	for (auto i : store)
-		cout << i << " ";
+void Reset() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
+			check[i][j] = 0;
+	}
 
-	return 0;
+	mp.clear();
+}
+
+void PrintCheck() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
+			cout << check[i][j] << " ";
+		cout << endl;
+	}
+}
+
+void PrintGraph() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cout << graph[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	Input();
+	int ans = 0;
+
+	while (1) {
+		NumberingMap();
+		TransferPeople();
+
+		if (check[N][N] == N * N)
+			break;
+
+		ans++;
+
+
+		Reset();
+	}
+
+	cout << ans << endl;
 }

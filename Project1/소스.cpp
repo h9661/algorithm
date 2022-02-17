f0ed016b54126d72941fe7583d7f1d2b73cffd5d
h9661 <guscksdn123@gmail.com>
2022-02-17 20:41:12 +0900

    codeforce Wrong Addition 구현

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,19 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void solve() {
+	long long a, s;
+	cin >> a >> s;
+
+	vector<int> b;
+
+	while (s) {
+		int x = a % 10;
+		int y = s % 10;
+
+		if (x <= y)
+			b.push_back(y - x);
+		else {
+			s /= 10;
+			y += 10 * (s % 10);
+
+			if (y >= 10 && y <= 19)
+				b.push_back(y - x);
+			else {
+				cout << "-1" << endl;
+				return;
+			}
+		}
+
+		a /= 10;
+		s /= 10;
+	}
+
+	if (a)
+		cout << "-1" << endl;
+	else {
+		while (b.back() == 0)
+			b.pop_back();
+		
+		for (int i = b.size() - 1; i >= 0; i--)
+			cout << b[i];
+		cout << endl;
+	}
+}
+
 int main() {
 	int t;
 	cin >> t;
 
 	while (t--) {
-		int n;
-		cin >> n;
-
-		set<int> a;
-		for (int i = 1; i * i <= n; i++)
-			a.insert(i * i);
-		for (int i = 1; i * i * i <= n; i++)
-			a.insert(i * i * i);
-		cout << a.size() << endl;
+		solve();
 	}
+
+	return 0;
 }

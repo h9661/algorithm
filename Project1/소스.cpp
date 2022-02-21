ecb55b7961a106f8452e356643156e5f2d5b452e
h9661 <guscksdn123@gmail.com>
2022-02-21 16:49:33 +0900

    codeforce Paint the Array 수학

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,54 +1,45 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-typedef long long ll;
-const int MAX_N = 2e5;
+long long gcd(long long a, long long b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	vector<long long> a(n);
+
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+
+	vector<long long> g(a.begin(), a.begin() + 2);
+
+	for (int i = 0; i < n; i++)
+		g[i % 2] = gcd(g[i % 2], a[i]);
+
+	vector<bool> good(2, true);
+
+	for (int i = 0; i < n; i++)
+		good[i % 2] = good[i % 2] && (a[i] % g[(i % 2) ^ 1] != 0);
+
+	long long ans = 0;
+
+	for (int i = 0; i < 2; i++) {
+		if (good[i])
+			ans = max(ans, g[i ^ 1]);
+	}
+
+	cout << ans << endl;
+}
 
 int main() {
 	int t;
 	cin >> t;
 
-	for (int _ = 0; _ < t; _++) {
-		int n;
-		cin >> n;
-
-		vector<int> a(n);
-		map<int, int> cnt;
-
-		for (int i = 0; i < n; i++) {
-			cin >> a[i];
-			cnt[a[i]]++;
-		}
-
-		sort(a.begin(), a.end());
-
-		stack<int> st;
-		vector<ll> ans(n + 1, -1);
-
-		ll sum = 0;
-
-		for (int i = 0; i <= n; i++) {
-			if (i > 0 && cnt[i - 1] == 0) {
-				if (st.empty())
-					break;
-
-				int j = st.top();
-				st.pop();
-				sum += i - j - 1;
-			}
-
-			ans[i] = sum + cnt[i];
-
-			while (i > 0 && cnt[i - 1] > 1) {
-				cnt[i - 1]--;
-				st.push(i - 1);
-			}
-		}
-
-		for (ll x : ans)
-			cout << x << " ";
-
-		cout << endl;
-	}
+	while (t--)
+		solve();
 }

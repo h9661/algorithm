4b11e5f8bf30194a41da88014eda047e608ef89c
h9661 <guscksdn123@gmail.com>
2022-02-22 16:04:54 +0900

    codeforces D. Array and Operations 수학, 그리디

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,39 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long gcd(long long a, long long b) {
-	if (b == 0)
-		return a;
-	else
-		return gcd(b, a % b);
-}
-
 void solve() {
-	int n;
-	cin >> n;
-	vector<long long> a(n);
+	int n, k;
+	cin >> n >> k;
+
+	vector<int> a(n);
 
 	for (int i = 0; i < n; i++)
 		cin >> a[i];
 
-	vector<long long> g(a.begin(), a.begin() + 2);
+	sort(a.begin(), a.end());
 
-	for (int i = 0; i < n; i++)
-		g[i % 2] = gcd(g[i % 2], a[i]);
-
-	vector<bool> good(2, true);
-
-	for (int i = 0; i < n; i++)
-		good[i % 2] = good[i % 2] && (a[i] % g[(i % 2) ^ 1] != 0);
-
-	long long ans = 0;
-
-	for (int i = 0; i < 2; i++) {
-		if (good[i])
-			ans = max(ans, g[i ^ 1]);
+	int score = accumulate(a.begin(), a.begin() + n - 2 * k, 0);
+	
+	for (int i = 0; i < k; i++) {
+		score += floor(a[i + n - 2 * k] / a[i + n - k]);
 	}
 
-	cout << ans << endl;
+	cout << score << endl;
 }
 
 int main() {
@@ -42,4 +27,6 @@ int main() {
 
 	while (t--)
 		solve();
+
+	return 0;
 }

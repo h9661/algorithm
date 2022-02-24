da828e91a6a6861cd3365ef8bd7d41fadfd9b78d
h9661 <guscksdn123@gmail.com>
2022-02-24 14:03:04 +0900

    codeforce B. Avoid Local Maximums 그리디

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -9,13 +9,27 @@ void solve() {
 	for (int i = 0; i < n; i++)
 		cin >> a[i];
 
-	int sum = a[0];
-
-	for (int i = 0; i < n; i++) {
-		sum |= a[i];
+	int count = 0;
+	for (int i = 1; i < n - 1; i++) {
+		if (i < n - 2) {
+			if (a[i] > a[i - 1] && a[i] > a[i + 1]) {
+				a[i + 1] = max(a[i], a[i + 2]);
+				count++;
+			}
+		}
+		
+		if (i == n - 2) {
+			if (a[i] > a[i - 1] && a[i] > a[i + 1]) {
+				a[i + 1] = a[i];
+				count++;
+			}
+		}
 	}
 
-	cout << sum << endl;
+	cout << count << endl;
+	for (int i : a)
+		cout << i << " ";
+	cout << endl;
 }
 
 int main() {

852a2b4724b91e820942f8805191a02ca6443186
h9661 <guscksdn123@gmail.com>
2022-03-02 15:17:42 +0900

    codeforces A - Hard Way geometry

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -2,41 +2,22 @@
 using namespace std;
 
 void solve() {
-	int n;
-	cin >> n;
+	pair<int, int> p1;
+	pair<int, int> p2;
+	pair<int, int> p3;
 
-	vector<long long> a(n);
-	int count = 0;
+	cin >> p1.first; cin >> p1.second;
+	cin >> p2.first; cin >> p2.second;
+	cin >> p3.first; cin >> p3.second;
 
-
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
-
-	if (a[n - 2] > a[n - 1]) {
-		cout << -1 << endl;
-		return;
-	}
-	else {
-		if (a[n - 1] >= 0) {
-			for (int i = 0; i < n - 2; i++) {
-				a[i] = a[n - 2] - a[n - 1];
-				count++;
-			}
-			
-			cout << count << endl;
-			for (int i = 1; i <= n - 2; i++)
-				cout << i << " " << n - 1 << " " << n << endl;
-		}
-		else {
-			if (is_sorted(a.begin(), a.end(), [](long long a, long long b) {
-				return a < b;
-				})) {
-				cout << count << endl;
-			}
-			else
-				cout << -1 << endl;
-		}
-	}
+	if (p1.second == p2.second && p1.second > p3.second)
+		cout << abs(p1.first - p2.first) << endl;
+	else if (p1.second == p3.second && p1.second > p2.second)
+		cout << abs(p1.first - p3.first) << endl;
+	else if (p2.second == p3.second && p2.second > p1.second)
+		cout << abs(p2.first - p3.first) << endl;
+	else
+		cout << "0" << endl;
 }
 
 int main() {

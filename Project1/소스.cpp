0719e12f199b939db9628412a2e010188de89ae6
h9661 <guscksdn123@gmail.com>
2022-03-03 11:21:24 +0900

    codeforces B - Power Walking Greedy

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -2,22 +2,23 @@
 using namespace std;
 
 void solve() {
-	pair<int, int> p1;
-	pair<int, int> p2;
-	pair<int, int> p3;
+	int n;
+	cin >> n;
 
-	cin >> p1.first; cin >> p1.second;
-	cin >> p2.first; cin >> p2.second;
-	cin >> p3.first; cin >> p3.second;
+	map<int, int> d;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		d[x]++;
+	}
 
-	if (p1.second == p2.second && p1.second > p3.second)
-		cout << abs(p1.first - p2.first) << endl;
-	else if (p1.second == p3.second && p1.second > p2.second)
-		cout << abs(p1.first - p3.first) << endl;
-	else if (p2.second == p3.second && p2.second > p1.second)
-		cout << abs(p2.first - p3.first) << endl;
-	else
-		cout << "0" << endl;
+	int cnt = 0;
+	for (auto i : d)
+		cnt++;
+
+	for (int k = 1; k <= n; k++)
+		cout << max(k, cnt) << " ";
+	cout << endl;
 }
 
 int main() {

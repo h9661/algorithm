be41238b35e23f55f30dba0a3cd1540b2a76d3b6
h9661 <guscksdn123@gmail.com>
2022-03-15 21:54:39 +0900

    codeforces A - Make Even math, implementation

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -3,42 +3,43 @@
 #define y second
 using namespace std;
 
-struct point {
-	int weight, position, id;
-};
-
 void solve() {
-	int n, m;
-	cin >> n >> m;
-	
-	vector<point> points(m);
-	for (int i = 0; i < m; i++) {
-		cin >> points[i].position >> points[i].weight;
-		points[i].id = i + 1;
-	}
+	string s;
+	cin >> s;
 
-	sort(points.begin(), points.end(), [](point a, point b) {
-		return a.weight < b.weight;
-		}
-	);
+	int ans = 0;
 
-	int sum = 0;
-	
-	for (int i = 0; i < m; i++) {
-		if (i < 2 * n)
-			sum += points[i].weight;
+	if (s.size() == 1) {
+		if ((s[0] - '0') % 2 == 0)
+			ans = 0;
 		else
-			points.pop_back();
+			ans = -1;
+	}
+	else {
+		if ((s[s.size() - 1] - '0') % 2 == 0)
+			ans = 0;
+		else {
+			if ((s[0] - '0') % 2 == 0)
+				ans = 1;
+			else {
+				bool check = false;
+
+				for (int i = 1; i < s.size() - 1; i++) {
+					if ((s[i] - '0') % 2 == 0) {
+						check = true;
+						break;
+					}
+				}
+
+				if (check)
+					ans = 2;
+				else
+					ans = -1;
+			}
+		}
 	}
 
-	sort(points.begin(), points.end(), [](point a, point b) {
-		return a.position < b.position;
-		}
-	);
-
-	cout << sum << endl;
-	for (int i = 0; i < n; i++)
-		cout << points[i].id << ' ' << points[2 * n - i - 1].id << endl;
+	cout << ans << endl;
 }
 
 int main() {

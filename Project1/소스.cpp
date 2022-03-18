e64f52c0014b7f78ffd02d105941dad30b39536b
h9661 <guscksdn123@gmail.com>
2022-03-18 13:58:21 +0900

    codeforces C - Polycarp Recovers the Permutation thinking

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,51 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string str;
-int ans = 0;
-map<int, bool> mp;
+void solve() {
+	int n;
+	cin >> n;
 
-void dfs(string numbers, vector<bool>& check) {
-    if (str.size() > 0) {
-        int num = stoi(str);
-        bool isPrime = true;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
 
-        if (mp[num] == false)
-            mp[num] = true;
-        else
-            isPrime = false;
-
-        if (num < 2)
-            isPrime = false;
-
-        for (int i = 2; i <= num - 1; i++) {
-            if (num % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-
-        if (isPrime)
-            ans++;
-    }
-
-    for (int i = 0; i < numbers.size(); i++) {
-        if (check[i] == false) {
-            check[i] = true;
-            str.push_back(numbers[i]);
-
-            dfs(numbers, check);
-
-            check[i] = false;
-            str.pop_back();
-        }
-    }
+	if (a[0] != n && a[n - 1] != n)
+		cout << -1 << endl;
+	else {
+		for (int i = n - 1; i >= 0; i--)
+			cout << a[i] << " ";
+		cout << endl;
+	}
 }
 
-int solution(string numbers) {
-    vector<bool> check(numbers.size(), false);
+int main() {
+	int t;
+	cin >> t;
 
-    dfs(numbers, check);
-
-    return ans;
+	while (t--) {
+		solve();
+	}
 }

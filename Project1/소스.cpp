170e1aeb55e2ff76dab09f45dbe5c5b4b8a1efe0
h9661 <guscksdn123@gmail.com>
2022-03-30 00:11:11 +0900

    Programmers 다리를 지나는 트럭 simulation, queue

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,19 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solution(vector<vector<string>> clothes) {
-    int answer = 1;
+void shiftLeft(vector<int>& arr) {
+    for (int i = 0; i < arr.size() - 1; i++) {
+        arr[i] = arr[i + 1];
+        arr[i + 1] = 0;
+    }
+}
 
-    map<string, int> mp;
-    for (int i = 0; i < clothes.size(); i++) {
-        mp[clothes[i][1]]++;
+bool isEmpty(vector<int> arr) {
+    bool check = true;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] != 0) {
+            check = false;
+            break;
+        }
     }
 
-    map<string, int>::iterator iter;
+    return check;
+}
 
-    for (iter = mp.begin(); iter != mp.end(); iter++) {
-        answer *= (iter->second + 1);
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int time = 0;
+    vector<int> arr(bridge_length + 1, 0);
+
+    queue<int> q;
+    for (int i = 0; i < truck_weights.size(); i++)
+        q.push(truck_weights[i]);
+
+    int curWeight = 0;
+
+    while (1) {
+
+        if (!q.empty()) {
+            if (curWeight + q.front() <= weight) {
+                curWeight += q.front();
+                arr[bridge_length] = q.front();
+
+                q.pop();
+            }
+        }
+
+        shiftLeft(arr);
+        time++;
+
+        if (arr[0] != 0) {
+            curWeight -= arr[0];
+            arr[0] = 0;
+        }
+
+        if (isEmpty(arr) && q.empty())
+            break;
     }
 
-    return answer - 1;
+    return time + 1;
+}
+
+int main() {
+    vector<int> arr = { 7, 4, 5, 6 };
+    cout << solution(2, 10, arr) << endl;
 }

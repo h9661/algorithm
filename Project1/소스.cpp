bed001b82b75997582aab6f17951ceff4b9ac1f1
h9661 <guscksdn123@gmail.com>
2022-04-06 00:12:10 +0900

    BackJoon 1339 Greedy, math

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,31 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int coefficient[26];
+
 int main() {
 	int n;
 	cin >> n;
 
-	priority_queue<int, vector<int>, greater<int>> pq;
+	vector<string> arr(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		int level = pow(10, arr[i].size());
+
+		for (int j = 0; j < arr[i].size(); j++) {
+			coefficient[arr[i][j] - 'A'] += level;
+			level /= 10;
+		}
+	}
+
+	vector<pair<char, int>> countArr;
+	for (int i = 0; i < 26; i++) {
+		if (coefficient[i] != 0) {
+			countArr.push_back({ i + 'A' , coefficient[i] });
+		}
+	}
+
+	sort(countArr.begin(), countArr.end(), [](pair<char, int> a, pair<char, int> b){
+		return a.second > b.second;
+		});
+
+	map<char, int> mp;
+
+	int k = 9;
+	for (int i = 0; i < countArr.size(); i++) {
+		mp[countArr[i].first] = k--;
+	}
+
 
 	for (int i = 0; i < n; i++) {
-		int temp;
-		cin >> temp;
-
-		pq.push(temp);
-	}
-
-	if (n >= 2) {
-		int sum = 0;
-
-		for (int i = 0; i < n - 1; i++) {
-			int a = pq.top(); pq.pop();
-			int b = pq.top(); pq.pop();
-			sum += a + b;
-			pq.push(a + b);
+		for (int j = 0; j < arr[i].size(); j++) {
+			arr[i][j] = mp[arr[i][j]] + '0';
 		}
-
-		cout << sum << endl;
 	}
-	else
-		cout << 0 << endl;
+	int sum = 0;
+	for (int i = 0; i < arr.size(); i++)
+		sum += stoi(arr[i]);
+
+	cout << sum << endl;
 }

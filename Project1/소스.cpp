a719f2f5a7333e16e0f1907296f771738bafd4cc
h9661 <guscksdn123@gmail.com>
2022-04-13 22:27:58 +0900

    Programmers 네트워크 DFS/BFS graph searching

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,16 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solution(vector<int> citations) {
-    int answer = 0;
-    sort(citations.begin(), citations.end(), greater<int>());
+void bfs(vector<int> graph[], int startloc, const int n, vector<bool>& check) {
+    queue<int> q;
 
-    for (int i = 0; i < citations.size(); i++) {
-        if (citations[i] > i)
-            answer++;
-        else
-            break;
+    q.push(startloc);
+    check[startloc] = true;
+
+    while (!q.empty()) {
+        int curNode = q.front();
+        q.pop();
+
+        for (int i = 0; i < graph[curNode].size(); i++) {
+            int nextNode = graph[curNode][i];
+
+            if (check[nextNode] == false) {
+                check[nextNode] = true;
+                q.push(nextNode);
+            }
+        }
     }
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+
+    vector<bool> check(n, false);
+    vector<int> graph[n];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (computers[i][j] == 1) {
+                graph[i].push_back(j);
+                graph[j].push_back(i);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (check[i] == false) {
+            bfs(graph, i, n, check);
+            answer++;
+        }
+    }
+
 
     return answer;
 }

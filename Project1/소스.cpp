8259894ef86464b3fecbbab6cb50b05eac5889e2
h9661 <guscksdn123@gmail.com>
2022-04-19 22:12:29 +0900

    BackJoon 11399 sorting, greedy

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,25 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int memo[5001];
-const int INF = 1e8;
 int main() {
-	int N;
-	cin >> N;
+	int n;
+	cin >> n;
 
-	memo[0] = 0;
-	memo[1] = INF;
-	memo[2] = INF;
-	memo[3] = 1;
-	memo[4] = INF;
-	memo[5] = 1;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
 
-	for (int i = 6; i <= 5000; i++) {
-		memo[i] = min(memo[i - 3] + 1, memo[i - 5] + 1);
+	sort(a.begin(), a.end());
+
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= i; j++) {
+			sum += a[j];
+		}
 	}
 
-	if (memo[N] >= 50000)
-		cout << -1 << endl;
-	else
-		cout << memo[N] << endl;
+	cout << sum << endl;
 }

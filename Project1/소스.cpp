ae00f1be2ad1f3e69c7920286404ac6fb5c76bf4
h9661 <guscksdn123@gmail.com>
2022-04-19 23:04:38 +0900

    BackJoon 2630 D&Q

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,22 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int n;
-	cin >> n;
+const int MAX = 129;
+int paper[MAX][MAX];
+int white = 0;
+int blue = 0;
 
-	vector<int> a(n);
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
+bool checking(int n, int x, int y) {
+	int temp = paper[y][x];
 
-	sort(a.begin(), a.end());
-
-	int sum = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j <= i; j++) {
-			sum += a[j];
+	for (int i = y; i < n + y; i++) {
+		for (int j = x; j < n + x; j++) {
+			if (paper[i][j] != temp)
+				return false;
 		}
 	}
 
-	cout << sum << endl;
+	return true;
+}
+
+void devide_paper(int n, int x, int y) {
+	if (!checking(n, x, y)) {
+		devide_paper(n / 2, x, y);
+		devide_paper(n / 2, x + n / 2, y);
+		devide_paper(n / 2, x, y + n / 2);
+		devide_paper(n / 2, x + n / 2, y + n / 2);
+	}
+	else {
+		if (paper[y][x] == 1)
+			blue++;
+		else
+			white++;
+	}
+}
+
+int main() {
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
+			cin >> paper[i][j];
+	}
+
+	devide_paper(N, 1, 1);
+
+	cout << white << endl;
+	cout << blue << endl;
 }

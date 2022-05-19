d652a16e42075a4851f3a5ebdc5ca54335d9c3d9
h9661 <guscksdn123@gmail.com>
2022-05-19 21:54:55 +0900

    backjoon 1654 이분탐색

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -2,21 +2,28 @@
 using namespace std;
 
 int main() {
-	int N;
-	cin >> N;
-	int count = 0;
-	int number = 1;
+	int K, N;
+	cin >> K >> N;
 
-	while (count <= 10000) {
-		if (to_string(number).find("666") != string::npos) {
-			count++;
-		}
+	vector<int> arr(K, 0);
+	for (int i = 0; i < K; i++)
+		cin >> arr[i];
 
-		if (count == N) {
-			cout << number << endl;
-			break;
-		}
+	long long left = 1;
+	long long right = *max_element(arr.begin(), arr.end());
 
-		number++;
+	while (left <= right) {
+		long long cnt = 0;
+		long long middle = (left + right) / 2;
+
+		for (int i = 0; i < arr.size(); i++)
+			cnt += arr[i] / middle;
+		
+		if (cnt >= N)
+			left = middle + 1;
+		else
+			right = middle - 1;
 	}
+
+	cout << left - 1 << endl;
 }

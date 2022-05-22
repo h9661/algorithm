3cdfb3c828ebdfaee267cae49970dfbe77adef87
h9661 <guscksdn123@gmail.com>
2022-05-22 12:56:09 +0900

    backjoon 1978

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -2,45 +2,32 @@
 #define endl '\n'
 using namespace std;
 
-int main() {
-	int tc;
-	cin >> tc;
-	while (tc--) {
-		int N, M;
-		cin >> N >> M;
+vector<int> primeNumber(1001);
+void eratos() {
+	for (int i = 2; i <= 1000; i++)
+		primeNumber[i] = i;
 
-		deque<int> q;
-		for (int i = 0; i < N; i++) {
-			int temp;
-			cin >> temp;
-
-			q.push_back(temp);
-		}
-
-		int ans = 0;
-		int idx = M;
-
-		while (1) {
-			if (q.front() == *max_element(q.begin(), q.end())) {
-				q.pop_front();
-				ans++;
-
-				if (idx == 0) {
-					cout << ans << endl;
-					break;
-				}
-				else
-					idx -= 1;
-			}
-			else {
-				q.push_back(q.front());
-				q.pop_front();
-
-				if (idx == 0)
-					idx = q.size() - 1;
-				else
-					idx -= 1;
-			}
+	for (int i = 2; i <= 1000; i++) {
+		for (int j = 2 * i; j <= 1000; j += i) {
+			primeNumber[j] = 0;
 		}
 	}
+}
+
+int main() {
+	int N;
+	cin >> N;
+	eratos();
+	vector<int> arr(N, 0);
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	int count = 0;
+
+	for (int i = 0; i < arr.size(); i++) {
+		if (primeNumber[arr[i]] != 0)
+			count++;
+	}
+
+	cout << count << endl;
 }

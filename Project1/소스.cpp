de8fb70ade32f409f4dbb9ea9843a77ab703e23e
h9661 <guscksdn123@gmail.com>
2022-05-25 20:48:00 +0900

    backjoon 10814 stablesort

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -3,31 +3,22 @@
 using namespace std;
 
 int main() {
-	int N, M;
-	cin >> N >> M;
+	int n;
+	cin >> n;
 
-	vector<int> arr(N, 0);
-	for (int i = 0; i < N; i++)
-		cin >> arr[i];
+	vector<pair<int, string>> members;
+	for (int i = 0; i < n; i++) {
+		int age; string name;
+		cin >> age >> name;
 
-	int sum = 0;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (i == j)
-				continue;
-			else {
-				for (int k = 0; k < N; k++) {
-					if (i == k or j == k)
-						continue;
-					else {
-						if (sum < arr[i] + arr[j] + arr[k] && arr[i] + arr[j] + arr[k] <= M)
-							sum = arr[i] + arr[j] + arr[k];
-					}
-				}
-			}
-		}
+		members.push_back({ age, name });
 	}
 
-	cout << sum << endl;
+	stable_sort(members.begin(), members.end(), [](pair<int, string> a, pair<int, string> b) {
+		return a.first < b.first;
+		}
+	);
+
+	for (auto& i : members)
+		cout << i.first << " " << i.second << endl;	
 }

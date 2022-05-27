6bcb8a8dcb639745d985e90b96b58c4c6abd2945
h9661 <guscksdn123@gmail.com>
2022-05-27 16:07:18 +0900

    backjoon 2869

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -2,26 +2,16 @@
 #define endl '\n'
 using namespace std;
 
-int dp[5001];
-const int INF = 1e9;
-
 int main() {
-	int N;
-	cin >> N;
+	int A, B, V;
+	cin >> A >> B >> V;
 
-	dp[1] = INF;
-	dp[2] = INF;
-	dp[3] = 1;
-	dp[4] = INF;
-	dp[5] = 1;
+	int answer = 0;
+	answer += (V - A) / (A - B);
+	answer += 1;
 
+	if (((V - A) % (A - B)) != 0)
+		answer += 1;
 
-	for (int i = 6; i <= 5000; i++) {
-		dp[i] = min(dp[i - 3] + 1, dp[i - 5] + 1);
-	}
-
-	if (dp[N] >= 1e8)
-		cout << -1 << endl;
-	else
-		cout << dp[N] << endl;
+	cout << answer << endl;
 }

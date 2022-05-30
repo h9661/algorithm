31445ce12caacf15a7c07de9a0d92b93c7b6c75e
h9661 <guscksdn123@gmail.com>
2022-05-30 13:33:24 +0900

    backjoon 1003

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -2,61 +2,30 @@
 #define endl '\n'
 using namespace std;
 
-const int INF = 1e9;
+#define zero first
+#define one second
 
 int main() {
-	int M, N, B;
-	cin >> N >> M >> B;
+	int T;
+	cin >> T;
 
-	vector<vector<int>> arr(N, vector<int>(M, 0));
+	vector<pair<int, int>> dp(41);
+	dp[0].zero = 1;
+	dp[0].one = 0;
 
-	int max_value = 0;
-	int min_value = INF;
+	dp[1].zero = 0;
+	dp[1].one = 1;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> arr[i][j];
-
-			if (arr[i][j] > max_value)
-				max_value = arr[i][j];
-			
-			if (arr[i][j] < min_value)
-				min_value = arr[i][j];
-		}
+	for (int i = 2; i <= 40; i++) {
+		dp[i].zero = dp[i - 1].zero + dp[i - 2].zero;
+		dp[i].one = dp[i - 1].one + dp[i - 2].one;
 	}
 
-	vector<pair<int, int>> answer;
 
-	for (int h = min_value; h <= max_value; h++) {
-		int time = 0;
-		int height = h;
-		int block = B;
+	while (T--) {
+		int n;
+		cin >> n;
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (arr[i][j] > h) {
-					time += (arr[i][j] - h) * 2;
-					block += arr[i][j] - h;
-				}
-
-				if (arr[i][j] < h) {
-					time += (h - arr[i][j]);
-					block -= h - arr[i][j];
-				}
-			}
-		}
-
-		if (block >= 0) {
-			answer.push_back({ time, height });
-		}
+		cout << dp[n].zero << " " << dp[n].one << endl;
 	}
-
-	sort(answer.begin(), answer.end(), [](pair<int, int> a, pair<int, int> b) {
-		if (a.first != b.first)
-			return a.first < b.first;
-		else
-			return a.second > b.second;
-		});
-
-	cout << answer[0].first << " " << answer[0].second << endl;
 }

2aa7f67c1762dbf5a1308736b9bc7c3f397e9dd8
h9661 <guscksdn123@gmail.com>
2022-05-31 11:51:03 +0900

    backjoon 1260 dfs/bfs

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -2,28 +2,63 @@
 #define endl '\n'
 using namespace std;
 
-int g_count = 0;
-int g_r, g_c;
+bool check[1001];
 
-void DC(int r, int c, int N, int num) {
-	if (abs(r - g_r) + abs(c - g_c) > pow(2, N + 1))
-		return;
-
-	if (N == 0) {
-		if (r == g_r and c == g_c)
-			cout << num << endl;
-	}
-	else {
-		DC(r, c, N - 1, num);
-		DC(r, c + pow(2, N - 1), N - 1, num + pow(4, N - 1) * 1);
-		DC(r + pow(2, N - 1), c, N - 1, num + pow(4, N - 1) * 2);
-		DC(r + pow(2, N - 1), c + pow(2, N - 1), N - 1, num + pow(4, N - 1) * 3);
+void dfs(int N, int V, vector<vector<int>> graph) {
+	cout << V << " ";
+	check[V] = true;
+	for (int i = 0; i < graph[V].size(); i++) {
+		if (check[graph[V][i]] == false) {
+			check[graph[V][i]] = true;
+			dfs(N, graph[V][i], graph);
+		}
 	}
 }
 
-int main() {
-	int N;
-	cin >> N >> g_r >> g_c;
+void bfs(int N, int V, vector<vector<int>> graph) {
+	vector<bool> check(N + 1, false);
+	queue<int> que;
 
-	DC(0, 0, N, 0);
+	que.push(V);
+	check[V] = true;
+
+	while (!que.empty()) {
+		int curNode = que.front();
+		cout << curNode << " ";
+		que.pop();
+
+		for (int i = 0; i < graph[curNode].size(); i++) {
+			int nxtNode = graph[curNode][i];
+
+			if (check[nxtNode] == false) {
+				que.push(nxtNode);
+				check[nxtNode] = true;
+			}
+		}
+	}
+	cout << endl;
+}
+
+int main() {
+	int N, M, V;
+	cin >> N >> M >> V;
+
+	vector<vector<int>> graph(N + 1);
+
+	for (int i = 0; i < M; i++) {
+		int u, v;
+		cin >> u >> v;
+
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+
+	for (int i = 1; i <= N; i++){
+		sort(graph[i].begin(), graph[i].end());
+	}
+
+	dfs(N, V, graph);
+	cout << endl;
+	bfs(N, V, graph);
 }

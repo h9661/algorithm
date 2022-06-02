d3d969554e80ed4abe14c0ee50a05bcd8ea096ff
h9661 <guscksdn123@gmail.com>
2022-06-02 21:01:16 +0900

    backjoon 1463 dynamic programming

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -2,45 +2,26 @@
 #define endl '\n'
 using namespace std;
 
-const int INF = 1e9;
 
 int main() {
-	int N, M;
-	cin >> N >> M;
+	int N;
+	cin >> N;
 
-	vector<vector<int>> graph(N + 1, vector<int>(N + 1, INF));
-	for (int i = 0; i < M; i++) {
-		int u, v;
-		cin >> u >> v;
+	vector<int> dp(1000001, 1000000);
 
-		graph[u][v] = 1;
-		graph[v][u] = 1;
+	dp[1] = 0;
+	dp[2] = 1;
+	dp[3] = 1;
+
+	for (int i = 4; i <= 1000000; i++) {
+		if (i % 3 == 0)
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+
+		if (i % 2 == 0)
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+
+		dp[i] = min(dp[i], dp[i - 1] + 1);
 	}
 
-	for (int k = 1; k <= N; k++) {
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if (graph[i][k] + graph[k][j] < graph[i][j])
-					graph[i][j] = graph[i][k] + graph[k][j];
-			}
-		}
-	}
-
-	vector<pair<int, int>> answer;
-
-	for (int i = 1; i <= N; i++) {
-		int sum = 0;
-		for (int j = 1; j <= N; j++) {
-			if (i == j)
-				continue;
-			sum += graph[i][j];
-		}
-		answer.push_back({ i, sum });
-	}
-
-	sort(answer.begin(), answer.end(), [](pair<int, int> a, pair<int, int> b) {
-		return a.second < b.second;
-		});
-
-	cout << answer[0].first << endl;
+	cout << dp[N] << endl;
 }

2c5f4bddaac601ff1dc3a7539387857d9c870883
h9661 <guscksdn123@gmail.com>
2022-06-07 23:28:14 +0900

    backjoon 11723 set

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,50 +1,63 @@
 #include <bits/stdc++.h>
 #define endl '\n'
+#define ii pair<int, int>
 using namespace std;
 
-const int MAX = 100001;
-bool check[MAX];
-
 int main() {
-	int N, K;
-	cin >> N >> K;
+	ios::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
 
-	queue<pair<int, int>> q;
-	q.push({ N, 0 });
-	check[N] = true;
+	set<int> st;
+	int M;
+	cin >> M;
+	
+	for (int i = 0; i < M; i++) {
+		string s;
+		cin >> s;
 
-	int min_ans = 1e9;
+		if (s == "add") {
+			int x;
+			cin >> x;
 
-	while (!q.empty()) {
-		int curN = q.front().first;
-		int curC = q.front().second;
-		q.pop();
+			st.insert(x);
+		}
 
-		if (curN == K)
-			min_ans = min(min_ans, curC);
+		if (s == "remove") {
+			int x;
+			cin >> x;
 
-
-		if (2 * curN <= 100000 && 2 * curN >= 0) {
-			if (check[2 * curN] == false) {
-				q.push({ 2 * curN, curC + 1 });
-				check[2 * curN] = true;
+			if (st.count(x) != 0) {
+				st.erase(x);
 			}
 		}
 
-		if (curN + 1 <= 100000) {
-			if (check[curN + 1] == false) {
-				q.push({ curN + 1, curC + 1 });
-				check[1 + curN] = true;
+		if (s == "check") {
+			int x;
+			cin >> x;
+
+			if (st.count(x) != 0) {
+				cout << 1 << endl;
 			}
+			else
+				cout << 0 << endl;
 		}
 
-		if (curN - 1 >= 0) {
-			if (check[curN - 1] == false) {
-				q.push({ curN - 1, curC + 1 });
-				check[curN - 1] = true;
-			}
+		if (s == "toggle") {
+			int x;
+			cin >> x;
+
+			if (st.count(x) != 0)
+				st.erase(x);
+			else
+				st.insert(x);
 		}
+
+		if (s == "all") {
+			st = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14,15,16,17,18,19,20 };
+		}
+
+		if (s == "empty")
+			st.clear();
 	}
-
-	cout << min_ans << endl;
 }

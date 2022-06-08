e4fc7a2f4a1cdc1b0b97b52ea72e78b9c9e045e8
h9661 <guscksdn123@gmail.com>
2022-06-08 22:01:53 +0900

    backjoon 1780 분할정복, 재귀

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -3,61 +3,62 @@
 #define ii pair<int, int>
 using namespace std;
 
+int countzero = 0;
+int countone = 0;
+int countminusone = 0;
+int graph[3000][3000];
+
+void DC(int row, int col, int N) {
+	bool continueflag = false;
+	int temp = graph[row][col];
+
+	for (int i = row; i < N + row; i++) {
+		for (int j = col; j < N + col; j++) {
+			if (temp != graph[i][j]) {
+				continueflag = true;
+				break;
+			}
+		}
+	}
+
+	if (continueflag) {
+		DC(row, col, N / 3);
+		DC(row, col + N / 3, N / 3);
+		DC(row, col + (N / 3) * 2, N / 3);
+		DC(row + N / 3, col, N / 3);
+		DC(row + N / 3, col + N / 3, N / 3);
+		DC(row + N / 3, col + (N / 3) * 2, N / 3);
+		DC(row + (N / 3) * 2, col, N / 3);
+		DC(row + (N / 3) * 2, col + N / 3, N / 3);
+		DC(row + (N / 3) * 2, col + (N / 3) * 2, N / 3);
+	}
+	else {
+		if (temp == 1)
+			countone++;
+		else if (temp == 0)
+			countzero++;
+		else
+			countminusone++;
+	}
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cout.tie(NULL);
 	cin.tie(NULL);
 
-	set<int> st;
-	int M;
-	cin >> M;
-	
-	for (int i = 0; i < M; i++) {
-		string s;
-		cin >> s;
+	int N;
+	cin >> N;
 
-		if (s == "add") {
-			int x;
-			cin >> x;
-
-			st.insert(x);
-		}
-
-		if (s == "remove") {
-			int x;
-			cin >> x;
-
-			if (st.count(x) != 0) {
-				st.erase(x);
-			}
-		}
-
-		if (s == "check") {
-			int x;
-			cin >> x;
-
-			if (st.count(x) != 0) {
-				cout << 1 << endl;
-			}
-			else
-				cout << 0 << endl;
-		}
-
-		if (s == "toggle") {
-			int x;
-			cin >> x;
-
-			if (st.count(x) != 0)
-				st.erase(x);
-			else
-				st.insert(x);
-		}
-
-		if (s == "all") {
-			st = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14,15,16,17,18,19,20 };
-		}
-
-		if (s == "empty")
-			st.clear();
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
+			cin >> graph[i][j];
 	}
+
+
+	DC(1, 1, N);
+
+	cout << countminusone << endl;
+	cout << countzero << endl;
+	cout << countone << endl;
 }

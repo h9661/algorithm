e1a34cbce9cc4fe67bf2b727b57efa27491495d7
h9661 <guscksdn123@gmail.com>
2022-06-08 22:58:21 +0900

    backjoon 1927 우선순위큐

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -3,45 +3,6 @@
 #define ii pair<int, int>
 using namespace std;
 
-int countzero = 0;
-int countone = 0;
-int countminusone = 0;
-int graph[3000][3000];
-
-void DC(int row, int col, int N) {
-	bool continueflag = false;
-	int temp = graph[row][col];
-
-	for (int i = row; i < N + row; i++) {
-		for (int j = col; j < N + col; j++) {
-			if (temp != graph[i][j]) {
-				continueflag = true;
-				break;
-			}
-		}
-	}
-
-	if (continueflag) {
-		DC(row, col, N / 3);
-		DC(row, col + N / 3, N / 3);
-		DC(row, col + (N / 3) * 2, N / 3);
-		DC(row + N / 3, col, N / 3);
-		DC(row + N / 3, col + N / 3, N / 3);
-		DC(row + N / 3, col + (N / 3) * 2, N / 3);
-		DC(row + (N / 3) * 2, col, N / 3);
-		DC(row + (N / 3) * 2, col + N / 3, N / 3);
-		DC(row + (N / 3) * 2, col + (N / 3) * 2, N / 3);
-	}
-	else {
-		if (temp == 1)
-			countone++;
-		else if (temp == 0)
-			countzero++;
-		else
-			countminusone++;
-	}
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cout.tie(NULL);
@@ -50,15 +11,21 @@ int main() {
 	int N;
 	cin >> N;
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++)
-			cin >> graph[i][j];
+	priority_queue<int, vector<int>, greater<int>> pq;
+	for (int i = 0; i < N; i++) {
+		int x;
+		cin >> x;
+
+		if (x == 0) {
+			if (pq.size() == 0)
+				cout << 0 << endl;
+			else {
+				cout << pq.top() << endl;
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(x);
+		}
 	}
-
-
-	DC(1, 1, N);
-
-	cout << countminusone << endl;
-	cout << countzero << endl;
-	cout << countone << endl;
 }

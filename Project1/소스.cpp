e2c3a1f568d147658e39f67459e9a35beb5b1e5c
h9661 <guscksdn123@gmail.com>
2022-06-21 12:16:25 +0900

    backjoon 5525 문자열

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,96 +4,40 @@
 #define ii pair<int, int>
 using namespace std;
 
-vector<string> split(string str, char token) {
-	vector<string> retval;
-	
-	if (str == "")
-		return retval;
-
-	if (str.size() == 1) {
-		retval.push_back(str);
-		return retval;
-	}
-
-	int start = 0;
-	int d = str.find(token);
-
-	while (d != -1) {
-		retval.push_back(str.substr(start, d - start));
-		start = d + 1;
-		d = str.find(token, start);
-	}
-	retval.push_back(str.substr(start, d - start));
-
-	return retval;
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int t;
-	cin >> t;
+	int N;
+	cin >> N;
 
-	while (t--) {
-		string op;
-		cin >> op;
+	int M;
+	cin >> M;
 
-		int n;
-		cin >> n;
+	string s;
+	cin >> s;
 
-		string temp;
-		cin >> temp;
-		temp.pop_back();
-		temp.erase(temp.begin());
+	int count = 0;
 
-		vector<string> temparr = split(temp, ',');
+	for (int i = 0; i < M; i++) {
+		int k = 0;
 
-		deque<string> arr;
-		for (int i = 0; i < temparr.size(); i++)
-			arr.push_back(temparr[i]);
-
-		bool f = true;
-		bool contFlag = false;
-		for (int i = 0; i < op.size(); i++) {
-			if (op[i] == 'R')
-				f = !f;
-
-			if (op[i] == 'D') {
-				if (arr.size() == 0) {
-					cout << "error" << endl;
-					contFlag = true;
-					break;
-				}
-				else {
-					if (f == true)
-						arr.pop_front();
-					else
-						arr.pop_back();
-				}
-			}
-		}
-
-		if (contFlag)
+		if (s[i] == 'O')
 			continue;
-
-		if (arr.size() == 0) {
-			cout << "[]";
-		}
 		else {
-			if (f == true) {
-				cout << '[';
-				for (int i = 0; i < arr.size() - 1; i++)
-					cout << arr[i] << ",";
-				cout << arr[arr.size() - 1] << ']';
+			while (s[i + 1] == 'O' && s[i + 2] == 'I') {
+				k++;
+				if (k == N) {
+					k--;
+					count++;
+				}
+				
+				i += 2;
 			}
-			else {
-				cout << '[';
-				for (int i = arr.size() - 1; i >= 1; i--)
-					cout << arr[i] << ",";
-				cout << arr[0] << ']';
-			}
+
+			k = 0;
 		}
-		cout << endl;
 	}
+
+	cout << count << endl;
 }

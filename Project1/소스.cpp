a9d5d1ba73af221597365ae7b9c5db0191b21f08
h9661 <guscksdn123@gmail.com>
2022-06-23 09:31:31 +0900

    backjoon 11724 graph, bfs

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,23 +4,48 @@
 #define ii pair<int, int>
 using namespace std;
 
+const int MAX = 1001;
+vector<int> graph[MAX];
+bool check[MAX];
+
+void bfs(int node) {
+	queue<int> q;
+	q.push(node);
+	check[node] = true;
+
+	while (!q.empty()) {
+		int cn = q.front();
+		q.pop();
+
+		for (int i = 0; i < graph[cn].size(); i++) {
+			int nn = graph[cn][i];
+			
+			if (check[nn] == false) {
+				q.push(nn);
+				check[nn] = true;
+			}
+		}
+	}
+}
+
 int main() {
-	int N;
-	cin >> N;
+	int N, M;
+	cin >> N >> M;
+	int count = 0;
+	for (int i = 0; i < M; i++) {
+		int u, v;
+		cin >> u >> v;
 
-	vector<int> arr(N);
-	for (int i = 0; i < N; i++)
-		cin >> arr[i];
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
 
-	sort(arr.begin(), arr.end());
-
-	int sum = 0;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j <= i; j++) {
-			sum += arr[j];
+	for (int i = 1; i <= N; i++) {
+		if (check[i] == false) {
+			bfs(i);
+			count++;
 		}
 	}
 
-	cout << sum << endl;
+	cout << count << endl;
 }

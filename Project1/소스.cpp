242fdd49a23dd1572067a44982c8a0a3f98ce025
h9661 <guscksdn123@gmail.com>
2022-06-23 09:22:23 +0900

    backjoon 11399 sort

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -5,25 +5,22 @@
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
 	int N;
 	cin >> N;
-	vector<int> arr(N);
-	map<int, int> mp;
 
-	for (int i = 0; i < N; i++) {
+	vector<int> arr(N);
+	for (int i = 0; i < N; i++)
 		cin >> arr[i];
-	}
-	vector<int> temp = arr;
 
 	sort(arr.begin(), arr.end());
-	arr.erase(unique(arr.begin(), arr.end()), arr.end());
 
-	for (int i = 0; i < arr.size(); i++)
-		mp[arr[i]] = i;
+	int sum = 0;
 
-	for (int i = 0; i < temp.size(); i++)
-		cout << mp[temp[i]] << " ";
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= i; j++) {
+			sum += arr[j];
+		}
+	}
+
+	cout << sum << endl;
 }

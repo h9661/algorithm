4e39bd1b76c0d1dc3b813c08b74d9ceb67004a82
h9661 <guscksdn123@gmail.com>
2022-06-24 10:26:20 +0900

    backjoon 17219 map

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -7,24 +7,20 @@ using namespace std;
 int dp[50001];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	map<string, string> mp;
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		string site, password;
+		cin >> site >> password;
 
-	for (int i = 0; i <= 50000; i++)
-		dp[i] = 50000;
-
-	int n;
-	cin >> n;
-
-	dp[0] = 0;
-	dp[1] = 1;
-
-	for (int i = 2; i <= 50000; i++) {
-		for (int j = 1; j * j <= i; j++) {
-			dp[i] = min(dp[i], dp[i - j * j] + 1);
-		}
+		mp[site] = password;
 	}
 
-	cout << dp[n] << endl;
+	for (int i = 0; i < m; i++) {
+		string site;
+		cin >> site;
+
+		cout << mp[site] << endl;
+	}
 }

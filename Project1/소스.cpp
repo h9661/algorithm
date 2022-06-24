b8ee188792363a88f8b7df0b0301fae07ab4f1a3
h9661 <guscksdn123@gmail.com>
2022-06-24 10:47:27 +0900

    backjoon 11403 플로이드 워셜

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,23 +4,40 @@
 #define ii pair<int, int>
 using namespace std;
 
-int dp[50001];
+const int MAX = 101;
+const int INF = 1e9;
+int graph[MAX][MAX];
 
 int main() {
-	map<string, string> mp;
-	int n, m;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		string site, password;
-		cin >> site >> password;
+	int N;
+	cin >> N;
 
-		mp[site] = password;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			int x;
+			cin >> x;
+			if (x != 0)
+				graph[i][j] = x;
+			else
+				graph[i][j] = INF;
+		}
 	}
 
-	for (int i = 0; i < m; i++) {
-		string site;
-		cin >> site;
+	for (int k = 1; k <= N; k++) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (graph[i][j] > graph[i][k] + graph[k][j])
+					graph[i][j] = 1;
+			}
+		}
+	}
 
-		cout << mp[site] << endl;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (graph[i][j] == INF)
+				graph[i][j] = 0;
+			cout << graph[i][j] << " ";
+		}
+		cout << endl;
 	}
 }

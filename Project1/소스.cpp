f8241c7fd10fb7003b0c31244e197872f4dd9a0b
h9661 <guscksdn123@gmail.com>
2022-07-01 11:43:07 +0900

    backjoon 9019 bfs

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,55 +4,86 @@
 #define ii pair<int, int>
 using namespace std;
 
-int N, M;
-bool check[101];
 
 int main() {
-	cin >> N >> M;
-
-	map<int, int> warp;
-	for (int i = 0; i < N; i++) {
-		int x, y;
-		cin >> x >> y;
-
-		warp[x] = y;
-	}
-	for (int i = 0; i < M; i++) {
-		int u, v;
-		cin >> u >> v;
-
-		warp[u] = v;
-	}
-
-	queue<ii> q;
-	q.push({ 1, 0 });
-	check[1] = true;
-
-	int ans = 199999;
-
-	while (!q.empty()) {
-		int curpos = q.front().first;
-		int curCost = q.front().second;
-		q.pop();
-
-		if (curpos == 100)
-			ans = min(ans, curCost);
-
-		for (int i = 1; i <= 6; i++) {
-			int nxtpos = curpos + i;
-			int nxtcost = curCost + 1;
-
-			if (nxtpos <= 100 and check[nxtpos] == false) {
-				check[nxtpos] = true;
-				if (warp[nxtpos] != 0) {
-					q.push({ warp[nxtpos], nxtcost });
-					check[warp[nxtpos]] = true;
-				}
-				else
-					q.push({ nxtpos, nxtcost });
-			}
-		}
-	}
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	
-	cout << ans << endl;
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		vector<bool> check(10001, false);
+		queue<pair<int, string>> q;
+		int cur, target;
+		cin >> cur >> target;
+
+		q.push({ cur, "" });
+		check[cur] = true;
+
+		string ans = "2432432423423232323232323232323323";
+
+		while (!q.empty()) {
+			int curPos = q.front().first;
+			string curOp = q.front().second;
+			//cout << curPos << " " << curOp << endl;
+			q.pop();
+
+			if (curPos == target) {
+				if (ans.size() >= curOp.size())
+					ans = curOp;
+				break;
+			}
+
+			//D
+			int nxtPos = (curPos * 2) % 10000;
+			if (check[nxtPos] == false) {
+				q.push({ nxtPos, curOp + "D" });
+				check[nxtPos] = true;
+			}
+
+			//S
+			nxtPos = curPos - 1 < 0 ? 9999 : curPos - 1;
+			if (check[nxtPos] == false) {
+				q.push({ nxtPos, curOp + "S" });
+				check[nxtPos] = true;
+			}
+
+			//L
+			string temp = to_string(curPos);
+			if (temp.size() < 4) {
+				for (int i = 0; i < 4 - temp.size(); i++)
+					temp.insert(temp.begin(), '0');
+			}
+			char strtemp = temp[0];
+			for (int i = 0; i < temp.size() - 1; i++) {
+				temp[i] = temp[i + 1];
+			}
+			temp[temp.size() - 1] = strtemp;
+			nxtPos = stoi(temp);
+			if (check[nxtPos] == false) {
+				q.push({ nxtPos, curOp + "L" });
+				check[nxtPos] = true;
+			}
+
+			//R
+			temp = to_string(curPos);
+			if (temp.size() < 4) {
+				for (int i = 0; i < 4 - temp.size(); i++)
+					temp.insert(temp.begin(), '0');
+			}
+			char strTemp = temp[temp.size() - 1];
+			for (int i = temp.size() - 1; i >= 1; i--) {
+				temp[i] = temp[i - 1];
+			}
+			temp[0] = strTemp;
+			nxtPos = stoi(temp);
+			if (check[nxtPos] == false) {
+				q.push({ nxtPos, curOp + "R" });
+				check[nxtPos] = true;
+			}
+
+		}
+		cout << ans << endl;
+	}
 }

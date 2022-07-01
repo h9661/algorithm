7edaf31e2016ad27bd3bb6a61b523f0ae28d0335
h9661 <guscksdn123@gmail.com>
2022-07-01 09:27:20 +0900

    backjoon 16928 bfs

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,104 +4,55 @@
 #define ii pair<int, int>
 using namespace std;
 
-const int MAX = 100;
-int graphNomal[MAX][MAX];
-int graphNotNomal[MAX][MAX];
-bool check[MAX][MAX];
-int N;
-
-int Y[4] = { -1, 1, 0, 0 };
-int X[4] = { 0, 0, -1, 1 };
-
-void bfs1(int row, int col, int color) {
-	queue<ii> q;
-	q.push({ row, col });
-	check[row][col] = true;
-	
-	while (!q.empty()) {
-		int cy = q.front().first;
-		int cx = q.front().second;
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int ny = cy + Y[i];
-			int nx = cx + X[i];
-
-			if (0 <= ny and ny < N and 0 <= nx and nx < N) {
-				if (graphNomal[ny][nx] == color and check[ny][nx] == false) {
-					check[ny][nx] = true;
-					q.push({ ny,nx });
-				}
-			}
-		}
-	}
-}
-void bfs2(int row, int col, int color) {
-	queue<ii> q;
-	q.push({ row, col });
-	check[row][col] = true;
-
-	while (!q.empty()) {
-		int cy = q.front().first;
-		int cx = q.front().second;
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int ny = cy + Y[i];
-			int nx = cx + X[i];
-
-			if (0 <= ny and ny < N and 0 <= nx and nx < N) {
-				if (graphNotNomal[ny][nx] == color and check[ny][nx] == false) {
-					check[ny][nx] = true;
-					q.push({ ny,nx });
-				}
-			}
-		}
-	}
-}
+int N, M;
+bool check[101];
 
 int main() {
-	cin >> N;
+	cin >> N >> M;
 
+	map<int, int> warp;
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			char color;
-			cin >> color;
+		int x, y;
+		cin >> x >> y;
 
-			if (color == 'R') {
-				graphNomal[i][j] = 1;
-				graphNotNomal[i][j] = 1;
-			}
-			else if (color == 'G') {
-				graphNomal[i][j] = 2;
-				graphNotNomal[i][j] = 1;
-			}
-			else {
-				graphNomal[i][j] = 3;
-				graphNotNomal[i][j] = 2;
+		warp[x] = y;
+	}
+	for (int i = 0; i < M; i++) {
+		int u, v;
+		cin >> u >> v;
+
+		warp[u] = v;
+	}
+
+	queue<ii> q;
+	q.push({ 1, 0 });
+	check[1] = true;
+
+	int ans = 199999;
+
+	while (!q.empty()) {
+		int curpos = q.front().first;
+		int curCost = q.front().second;
+		q.pop();
+
+		if (curpos == 100)
+			ans = min(ans, curCost);
+
+		for (int i = 1; i <= 6; i++) {
+			int nxtpos = curpos + i;
+			int nxtcost = curCost + 1;
+
+			if (nxtpos <= 100 and check[nxtpos] == false) {
+				check[nxtpos] = true;
+				if (warp[nxtpos] != 0) {
+					q.push({ warp[nxtpos], nxtcost });
+					check[warp[nxtpos]] = true;
+				}
+				else
+					q.push({ nxtpos, nxtcost });
 			}
 		}
 	}
-
-	int count1 = 0;
-	int count2 = 0;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (check[i][j] == false) {
-				bfs1(i, j, graphNomal[i][j]);
-				count1++;
-			}
-		}
-	}
-	memset(check, false, sizeof(check));
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (check[i][j] == false) {
-				bfs2(i, j, graphNotNomal[i][j]);
-				count2++;
-			}
-		}
-	}
-
-	cout << count1 << " " << count2 << endl;
+	
+	cout << ans << endl;
 }

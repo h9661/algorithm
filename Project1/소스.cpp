8d444583d1f7ad7e7fe4abd4410ef8d50b17fc1c
h9661 <guscksdn123@gmail.com>
2022-07-04 14:52:05 +0900

    backjoon 15657 dfs

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,27 +4,39 @@
 #define ii pair<int, int>
 using namespace std;
 
-const int MAX = 1000;
-int dp[MAX][3];
-int arr[MAX][3];
+bool visited[9];
+vector<int> ans;
+vector<int> arr;
+int N, M;
+
+void dfs(int count) {
+	if (count == M) {
+		if (is_sorted(ans.begin(), ans.end())) {
+			for (auto i : ans)
+				cout << i << " ";
+			cout << endl;
+		}
+	}
+	else {
+		for (int i = 0; i < N; i++) {
+			ans.push_back(arr[i]);
+			dfs(count + 1);
+			ans.pop_back();
+		}
+	}
+}
 
 int main() {
-	int n;
-	cin >> n;
+	cin >> N >> M;
 
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
-	}
-	
-	dp[0][0] = arr[0][0];
-	dp[0][1] = arr[0][1];
-	dp[0][2] = arr[0][2];
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
 
-	for (int i = 1; i < n; i++) {
-		dp[i][0] = min(arr[i][0] + dp[i - 1][1], arr[i][0] + dp[i - 1][2]);
-		dp[i][1] = min(arr[i][1] + dp[i - 1][0], arr[i][1] + dp[i - 1][2]);
-		dp[i][2] = min(arr[i][2] + dp[i - 1][0], arr[i][2] + dp[i - 1][1]);
+		arr.push_back(temp);
 	}
 
-	cout << min(min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]) << endl;
+	sort(arr.begin(), arr.end());
+
+	dfs(0);
 }

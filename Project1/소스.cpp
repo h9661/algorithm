f82db72be94f702e26364be22a1fd644a201a0ac
h9661 <guscksdn123@gmail.com>
2022-07-04 09:56:57 +0900

    backjoon 15650 dfs

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,40 +4,40 @@
 #define ii pair<int, int>
 using namespace std;
 
+int N, M;
+vector<bool> check(9, false);
+vector<int> arr;
+
+void dfs(int k, int count) {
+	if (count == M) {
+		if (is_sorted(arr.begin(), arr.end())) {
+			for (auto i : arr)
+				cout << i << " ";
+
+			cout << endl;
+		}
+	}
+	else {
+		for (int i = k; i <= N; i++) {
+			if (check[i] == false) {
+				check[i] = true;
+
+				arr.push_back(i);
+				dfs(i + 1, count + 1);
+				arr.pop_back();
+				
+				check[i] = false;
+			}
+		}
+	}
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	long long A, B;
-	cin >> A >> B;
-
-	queue<ll> q;
-	q.push({ A, 0 });
-	map<long long, bool> mp;
-
-	while (!q.empty()) {
-		long long curX = q.front().first;
-		long long curC = q.front().second;
-		q.pop();
-
-		if (curX == B) {
-			cout << curC + 1 << endl;
-			return 0;
-		}
-
-		if (2 * curX <= B and mp[2 * curX] == false) {
-			q.push({ 2 * curX, curC + 1 });
-			mp[2 * curX] = true;
-		}
-
-		if (10 * curX + 1 <= B and mp[10 * curX + 1] == false) {
-			q.push({ curX * 10 + 1, curC + 1 });
-			mp[10 * curX + 1] = true;
-		}
-	}
-
-	cout << -1 << endl;
-	cout << "hello" << endl;
+	cin >> N >> M;
+	
+	dfs(1, 0);
 }

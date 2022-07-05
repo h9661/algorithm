17d3993c02fed141b42d439a800906b4d2cce39b
h9661 <guscksdn123@gmail.com>
2022-07-05 12:13:56 +0900

    backjoon 15663 dfs

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,39 +4,48 @@
 #define ii pair<int, int>
 using namespace std;
 
-bool visited[9];
-vector<int> ans;
-vector<int> arr;
+vector<bool> check(10001, false);
 int N, M;
+vector<int> arr;
+vector<int> ans;
 
 void dfs(int count) {
 	if (count == M) {
-		if (is_sorted(ans.begin(), ans.end())) {
+
 			for (auto i : ans)
 				cout << i << " ";
 			cout << endl;
-		}
 	}
 	else {
-		for (int i = 0; i < N; i++) {
-			ans.push_back(arr[i]);
-			dfs(count + 1);
-			ans.pop_back();
+		int k = 0;
+
+		for (int i = 0; i < arr.size(); i++) {
+			if (check[i] == false and arr[i] != k) {
+				check[i] = true;
+				k = arr[i];
+
+				ans.push_back(arr[i]);
+				dfs(count + 1);
+				ans.pop_back();
+
+				check[i] = false;
+			}
 		}
 	}
 }
 
 int main() {
 	cin >> N >> M;
-
+	
 	for (int i = 0; i < N; i++) {
-		int temp;
-		cin >> temp;
+		int x;
+		cin >> x;
 
-		arr.push_back(temp);
+		arr.push_back(x);
 	}
 
 	sort(arr.begin(), arr.end());
+
 
 	dfs(0);
 }

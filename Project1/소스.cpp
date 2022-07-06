58e6d6859efa5f73e46b2da2f7317c8e3b80625a
h9661 <guscksdn123@gmail.com>
2022-07-06 09:31:24 +0900

    backjoon 1700 greedy

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,48 +4,50 @@
 #define ii pair<int, int>
 using namespace std;
 
-int N, M;
-vector<int> arr;
-vector<bool> check(9);
-vector<int> ans;
-
-void dfs(int count) {
-	if (count == M) {
-		if (is_sorted(ans.begin(), ans.end())) {
-			for (auto i : ans)
-				cout << i << " ";
-			cout << endl;
-		}
-	}
-	else {
-		int k = 0;
-
-		for (int i = 0; i < N; i++) {
-			if (check[i] == false and arr[i] != k) {
-				check[i] = false;
-				k = arr[i];
-
-				ans.push_back(arr[i]);
-				dfs(count + 1);
-				ans.pop_back();
-
-				check[i] = false;
-			}
-		}
-	}
-}
-
 int main() {
-	cin >> N >> M;
+	int N, K;
+	cin >> N >> K;
+	int count = 0;
 
-	for (int i = 0; i < N; i++) {
-		int x;
-		cin >> x;
+	vector<int> multitap(K);
+	for (int i = 0; i < K; i++)
+		cin >> multitap[i];
 
-		arr.push_back(x);
+	vector<int> plug;
+
+	for (int i = 0; i < K; i++) {
+		if (find(plug.begin(), plug.end(), multitap[i]) != plug.end())
+			continue;
+
+		if (plug.size() < N) {
+			plug.push_back(multitap[i]);
+			continue;
+		}
+
+		vector<int> multitap_Idxs;
+		int multitap_idx = 0;
+		bool cont_flag = true;
+
+		for (int j = 0; j < N; j++) {
+			if (find(multitap.begin() + i, multitap.end(), plug[j]) != multitap.end())
+				multitap_idx = find(multitap.begin() + i, multitap.end(), plug[j]) - (multitap.begin() + i);
+			else {
+				multitap_idx = 101;
+				cont_flag = false;
+			}
+
+			multitap_Idxs.push_back(multitap_idx);
+
+			if (!cont_flag)
+				break;
+		}
+
+		int plugOut_idx = find(multitap_Idxs.begin(), multitap_Idxs.end(),
+			*max_element(multitap_Idxs.begin(), multitap_Idxs.end())) - multitap_Idxs.begin();
+		plug.erase(plug.begin() + plugOut_idx);
+		plug.push_back(multitap[i]);
+		count++;
 	}
 
-	sort(arr.begin(), arr.end());
-
-	dfs(0);
-}
+	cout << count << endl;
+} 

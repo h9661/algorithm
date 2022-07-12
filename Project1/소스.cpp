631f9552f84f947ad76bf5ddc75b2cacc3b01bf6
h9661 <guscksdn123@gmail.com>
2022-07-12 17:56:29 +0900

    backjoon 분할정복을 이용한 거듭제곱

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,52 +4,24 @@
 #define ii pair<int, int>
 using namespace std;
 
+long long pow(long long a, long long b, long long c) {
+	long long retval = 1;
+
+	while (b) {
+		if (b & 1)
+			retval *= a;
+
+		a *= a;
+		b /= 2;
+		retval %= c;
+	}
+
+	return retval;
+}
+
 int main() {
-	int N, K;
-	cin >> N >> K;
+	int a, b, c;
+	cin >> a >> b >> c;
 
-	vector<ii> arr;
-	for (int i = 0; i < N; i++) {
-		int w, v;
-		cin >> w >> v;
-
-		arr.push_back(make_pair(w, v));
-	}
-
-	vector<int> backpack;
-	for (int i = 0; i < K; i++) {
-		int x;
-		cin >> x;
-
-		backpack.push_back(x);
-	}
-
-	sort(arr.begin(), arr.end(), [](ii a, ii b) {
-		if (a.first != b.first)
-			return a.first < b.first;
-		else
-			return a.second > b.second;
-		}
-	);
-	sort(backpack.begin(), backpack.end(), [](int a, int b){
-		return a < b;
-		}
-	);
-
-	priority_queue<int, vector<int>, less<int>> pq;
-
-	int idx = 0;
-	long long ans = 0;
-
-	for (int i = 0; i < K; i++) {
-		while (idx < N and backpack[i] >= arr[idx].first)
-			pq.push(arr[idx++].second);
-
-		if (!pq.empty()) {
-			ans += pq.top();
-			pq.pop();
-		}
-	}
-
-	cout << ans << endl;
+	cout << pow(a, b, c) << endl;
 }

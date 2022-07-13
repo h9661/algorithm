b8edffe46686a05796396a59fcb1a60b81c38db5
h9661 <guscksdn123@gmail.com>
2022-07-13 13:24:00 +0900

    backjoon 2206 bfs

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,24 +4,74 @@
 #define ii pair<int, int>
 using namespace std;
 
-long long pow(long long a, long long b, long long c) {
-	long long retval = 1;
+int M, N;
+const int MAX = 1010;
+char graph[MAX][MAX];
+vector<int> resArr;
+bool visited[2][MAX][MAX];
+int Y[4] = { -1, 1, 0, 0 };
+int X[4] = { 0, 0, -1, 1 };
 
-	while (b) {
-		if (b & 1)
-			retval *= a;
+void bfs() {
+	queue<pair<ii, ii>> q;
+	q.push({ {0, 0}, {0, false} });
+	visited[0][0][0] = true;
 
-		a *= a;
-		b /= 2;
-		retval %= c;
+	while (!q.empty()) {
+		int cy = q.front().first.first;
+		int cx = q.front().first.second;
+		int cc = q.front().second.first;
+		int cb = q.front().second.second;
+		q.pop();
+
+		if (cy == M - 1 and cx == N - 1)
+			resArr.push_back(cc + 1);
+
+		for (int i = 0; i < 4; i++) {
+			int ny = cy + Y[i];
+			int nx = cx + X[i];
+			int nc = cc + 1;
+			int world = cb == false ? 0 : 1;
+
+			if (ny >= 0 and ny < M and nx >= 0 and nx < N) {
+				if (visited[world][ny][nx] == false) {
+					if (graph[ny][nx] == '1') {
+						if (cb == false) {
+							q.push({ {ny, nx}, {nc, true} });
+						}
+					}
+
+					if (graph[ny][nx] == '0') {
+						q.push({ {ny, nx}, {nc, cb} });
+						visited[world][ny][nx] = true;
+					}
+				}
+			}
+		}
 	}
-
-	return retval;
 }
 
 int main() {
-	int a, b, c;
-	cin >> a >> b >> c;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-	cout << pow(a, b, c) << endl;
+	cin >> M >> N;
+
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			char x;
+			cin >> x;
+
+			graph[i][j] = x;
+		}
+	}
+
+	bfs();
+
+	if (resArr.size() == 0)
+		cout << -1 << endl;
+	else
+		cout << *min_element(resArr.begin(), resArr.end()) << endl;
+	
 }

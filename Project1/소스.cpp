4a1bce7f7ca018ff5a30bcc78a788f0f47146a25
h9661 <guscksdn123@gmail.com>
2022-07-14 09:47:54 +0900

    backjoon 1644 eratos, two pointer

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,74 +4,57 @@
 #define ii pair<int, int>
 using namespace std;
 
-int M, N;
-const int MAX = 1010;
-char graph[MAX][MAX];
-vector<int> resArr;
-bool visited[2][MAX][MAX];
-int Y[4] = { -1, 1, 0, 0 };
-int X[4] = { 0, 0, -1, 1 };
+vector<int> arr(4000001);
 
-void bfs() {
-	queue<pair<ii, ii>> q;
-	q.push({ {0, 0}, {0, false} });
-	visited[0][0][0] = true;
+void eratos() {
+	for (int i = 2; i <= 4000000; i++)
+		arr[i] = i;
 
-	while (!q.empty()) {
-		int cy = q.front().first.first;
-		int cx = q.front().first.second;
-		int cc = q.front().second.first;
-		int cb = q.front().second.second;
-		q.pop();
+	for (int i = 2; i <= 4000000; i++) {
+		if (arr[i] == 0)
+			continue;
 
-		if (cy == M - 1 and cx == N - 1)
-			resArr.push_back(cc + 1);
-
-		for (int i = 0; i < 4; i++) {
-			int ny = cy + Y[i];
-			int nx = cx + X[i];
-			int nc = cc + 1;
-			int world = cb == false ? 0 : 1;
-
-			if (ny >= 0 and ny < M and nx >= 0 and nx < N) {
-				if (visited[world][ny][nx] == false) {
-					if (graph[ny][nx] == '1') {
-						if (cb == false) {
-							q.push({ {ny, nx}, {nc, true} });
-						}
-					}
-
-					if (graph[ny][nx] == '0') {
-						q.push({ {ny, nx}, {nc, cb} });
-						visited[world][ny][nx] = true;
-					}
-				}
-			}
-		}
+		for (int j = 2 * i; j <= 4000000; j += i)
+			arr[j] = 0;
 	}
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	eratos();
+	
+	int N;
+	cin >> N;
 
-	cin >> M >> N;
+	int left = N;
+	int right = N;
 
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
-			char x;
-			cin >> x;
+	int count = 0;
 
-			graph[i][j] = x;
+	while (left <= right and left >= 0) {
+		if (arr[left] == 0) {
+			left--;
+			continue;
 		}
+		if (arr[right] == 0) {
+			right--;
+			continue;
+		}
+
+		/*cout << left << " " << right << endl;
+		cout << arr[left] << " " << arr[right] << endl;*/
+
+		int sum = accumulate(arr.begin() + left, arr.begin() + right + 1, 0);
+		/*cout << sum << endl;*/
+		
+		if (sum <= N) {
+			left--;
+
+			if (sum == N)
+				count++;
+		}
+		else
+			right--;
 	}
 
-	bfs();
-
-	if (resArr.size() == 0)
-		cout << -1 << endl;
-	else
-		cout << *min_element(resArr.begin(), resArr.end()) << endl;
-	
+	cout << count << endl;
 }

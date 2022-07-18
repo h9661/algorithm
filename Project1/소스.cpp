bd99d17f11bbb73ae2a2fbd85428f8f44622e974
h9661 <guscksdn123@gmail.com>
2022-07-18 10:30:44 +0900

    backjoon 11660 dp

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,78 +4,34 @@
 #define ii pair<int, int>
 using namespace std;
 
-const int MAX = 801;
-vector<ii> graph[MAX];
-const int INF = 50000000;
-vector<long long> dist(MAX, INF);
+const int MAX = 1024 + 1;
+int chart[MAX][MAX];
+int dp[MAX][MAX];
 
-struct cmp {
-	bool operator()(ii a, ii b) {
-		return a.second > b.second;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
+			cin >> chart[i][j];
 	}
-};
 
-int djk(int source, int target) {
-	priority_queue<ii, vector<ii>, cmp> pq;
-	pq.push({ source, 0 });
-	dist[source] = 0;
-
-	while (!pq.empty()) {
-		int cn = pq.top().first;
-		int cc = pq.top().second;
-		pq.pop();
-
-		if (cn == target)
-			return dist[cn];
-
-		for (int i = 0; i < graph[cn].size(); i++) {
-			int nn = graph[cn][i].first;
-			int nc = graph[cn][i].second;
-
-			if (dist[nn] > dist[cn] + nc) {
-				dist[nn] = dist[cn] + nc;
-				pq.push({ nn, dist[nn]});
-			}
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + chart[i][j];
 		}
 	}
 
-	return INF;
-}
+	while (M--) {
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
 
-int main() {
-	int N, E;
-	cin >> N >> E;
+		cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << endl;
 
-	for (int i = 0; i < E; i++) {
-		int u, v, c;
-		cin >> u >> v >> c;
-
-		graph[u].push_back({ v, c });
-		graph[v].push_back({ u, c });
 	}
-
-	int v1, v2;
-	cin >> v1 >> v2;
-
-	long long sum1 = 0;
-	sum1 += djk(1, v1);
-	fill(dist.begin(), dist.end(), INF);
-	sum1 += djk(v1, v2);
-	fill(dist.begin(), dist.end(), INF);
-	sum1 += djk(v2, N);
-	fill(dist.begin(), dist.end(), INF);
-
-	long long sum2 = 0;
-	sum2 += djk(1, v2);
-	fill(dist.begin(), dist.end(), INF);
-	sum2 += djk(v2, v1);
-	fill(dist.begin(), dist.end(), INF);
-	sum2 += djk(v1, N);
-
-	long long ans = min(sum1, sum2);
-
-	if (ans >= 40000000)
-		cout << -1 << endl;
-	else
-		cout << ans << endl;
 }

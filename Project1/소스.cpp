862cece3d803bf0d94fbd19f5f73cf8ecb859af2
h9661 <guscksdn123@gmail.com>
2022-07-19 10:00:32 +0900

    backjoon 1932 dp

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,34 +4,24 @@
 #define ii pair<int, int>
 using namespace std;
 
-const int MAX = 1024 + 1;
-int chart[MAX][MAX];
-int dp[MAX][MAX];
+const int MAX = 500 + 1;
+int arr[MAX][MAX];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int N, M;
-	cin >> N >> M;
+	int N;
+	cin >> N;
 
 	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++)
-			cin >> chart[i][j];
-	}
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + chart[i][j];
+		for (int j = 1; j <= i; j++) {
+			cin >> arr[i][j];
 		}
 	}
 
-	while (M--) {
-		int x1, y1, x2, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
-
-		cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << endl;
-
+	for (int i = N - 1; i >= 1; i--) {
+		for (int j = 1; j <= i; j++) {
+			arr[i][j] += max(arr[i + 1][j], arr[i + 1][j + 1]);
+		}
 	}
+
+	cout << arr[1][1] << endl;
 }

9eb760065a059297909bc47a244fa5d5c771c81e
h9661 <guscksdn123@gmail.com>
2022-08-09 09:23:31 +0900

    backjoon 9663 backtracking

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -4,54 +4,35 @@
 #define ii pair<int, int>
 using namespace std;
 
-const int MAX = 10001;
-vector<ll> graph[MAX];
-bool check[MAX];
+int N;
+bool visited[30];
+bool flag_a[100];
+bool flag_b[100];
+int ans = 0;
 
-struct cmp {
-	bool operator()(ll a, ll b) {
-		return a.second > b.second;
+void dfs(int count) {
+	if (count == N) {
+		ans++;
 	}
-};
+	else {
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i] and !flag_a[i + count] and !flag_b[i - count + 7]) {
+				visited[i] = true;
+				flag_a[i + count] = true;
+				flag_b[i - count + 7] = true;
 
-int prim() {
-	long long retval = 0;
-	priority_queue<ll, vector<ll>, cmp> min_heap;
-	min_heap.push({ 1, 0 });
+				dfs(count + 1);
 
-	while (!min_heap.empty()) {
-		long long curNode = min_heap.top().first;
-		long long curCost = min_heap.top().second;
-		min_heap.pop();
-
-		if (check[curNode] == true)
-			continue;
-		check[curNode] = true;
-		retval += curCost;
-
-
-		for (int i = 0; i < graph[curNode].size(); i++) {
-			int nextNode = graph[curNode][i].first;
-			int nextCost = graph[curNode][i].second;
-
-			min_heap.push({ nextNode, nextCost });
+				visited[i] = false;
+				flag_a[i + count] = false;
+				flag_b[i - count + 7] = false;
+			}
 		}
 	}
-
-	return retval;
 }
 
 int main() {
-	int V, E;
-	cin >> V >> E;
-
-	for (int i = 0; i < E; i++) {
-		int u, v, c;
-		cin >> u >> v >> c;
-
-		graph[u].push_back({ v, c });
-		graph[v].push_back({ u, c });
-	}
-
-	cout << prim() << endl;
+	cin >> N;
+	dfs(0);
+	cout << ans << endl;
 }

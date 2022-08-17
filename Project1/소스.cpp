42ed5048429deb89493255ccfe9bac4e9843cd2d
h9661 <guscksdn123@gmail.com>
2022-08-17 14:08:30 +0900

    backjoon 2580 dfs, backtracking

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,38 +1,99 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
 #define endl '\n'
 #define ll pair<long long, long long>
 #define ii pair<int, int>
 using namespace std;
 
-int N;
-bool visited[30];
-bool flag_a[100];
-bool flag_b[100];
-int ans = 0;
+const int MAX = 9;
+vector<vector<int>> sudokuBoard(MAX, vector<int>(MAX));
+vector<vector<bool>> check(MAX, vector<bool>(MAX));
 
-void dfs(int count) {
-	if (count == N) {
-		ans++;
+bool CheckRow(int row, int columm, int number) {
+	for (int i = 0; i < 9; i++) {
+		if (columm == i)
+			continue;
+
+		if (number == sudokuBoard[row][i])
+			return false;
+	}
+
+	return true;
+}
+
+bool CheckColumm(int row, int columm, int number) {
+	for (int i = 0; i < 9; i++) {
+		if (row == i)
+			continue;
+
+		if (number == sudokuBoard[i][columm])
+			return false;
+	}
+
+	return true;
+}
+
+bool CheckSquare(int row, int columm, int number) {
+	int newRow = row / 3;
+	int newColumm = columm / 3;
+
+	for (int i = newRow * 3; i < (newRow + 1) * 3; i++) {
+		for (int j = newColumm * 3; j < (newColumm + 1) * 3; j++) {
+			if (row == newRow && columm == newColumm)
+				continue;
+
+			if (number == sudokuBoard[i][j])
+				return false;
+		}
+	}
+
+	return true;
+}
+
+void dfs(int pos) {
+	if (pos == 81) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++)
+				cout << sudokuBoard[i][j] << " ";
+			cout << endl;
+		}
+
+		exit(0);
 	}
 	else {
-		for (int i = 1; i <= N; i++) {
-			if (!visited[i] and !flag_a[i + count] and !flag_b[i - count + 7]) {
-				visited[i] = true;
-				flag_a[i + count] = true;
-				flag_b[i - count + 7] = true;
+		int row = pos / 9;
+		int columm = pos % 9;
 
-				dfs(count + 1);
+		if (check[row][columm] == false) {
+			check[row][columm] = true;
 
-				visited[i] = false;
-				flag_a[i + count] = false;
-				flag_b[i - count + 7] = false;
+			if (sudokuBoard[row][columm] == 0) {
+				for (int i = 1; i <= 9; i++) {
+					if (CheckRow(row, columm, i) && CheckColumm(row, columm, i) && CheckSquare(row, columm, i)) {
+						sudokuBoard[row][columm] = i;
+
+						dfs(pos + 1);
+
+						sudokuBoard[row][columm] = 0;
+					}
+				}
 			}
+			else
+				dfs(pos + 1);
+
+			check[row][columm] = false;
 		}
 	}
 }
 
 int main() {
-	cin >> N;
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	for (int i = 0; i < 9; i++)
+		for (int j = 0; j < 9; j++)
+			cin >> sudokuBoard[i][j];
+
 	dfs(0);
-	cout << ans << endl;
 }

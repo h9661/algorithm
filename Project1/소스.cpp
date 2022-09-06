9402126d6c6f8b872a64c59a2ca94f75d50f1f19
h9661 <guscksdn123@gmail.com>
2022-09-06 20:19:19 +0900

    Programmers 성격 유형 검사

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,19 +1,34 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int solution(vector<vector<int>> sizes) {
-    for (int i = 0; i < sizes.size(); i++) {
-        if (sizes[i][0] < sizes[i][1])
-            swap(sizes[i][0], sizes[i][1]);
+string solution(vector<string> survey, vector<int> choices) {
+    map<char, int> scorer;
+
+    for (int i = 0; i < choices.size(); i++) {
+        if (choices[i] > 4) {
+            scorer[survey[i][1]] += choices[i] - 4;
+        }
+        if (choices[i] < 4) {
+            scorer[survey[i][0]] += 4 - choices[i];
+        }
     }
 
-    int row = 0;
-    int col = 0;
+    string answer = "";
 
-    for (int i = 0; i < sizes.size(); i++) {
-        row = max(row, sizes[i][0]);
-        col = max(col, sizes[i][1]);
+    vector<string> arr = { "RT", "CF", "JM", "AN" };
+    for (int i = 0; i < arr.size(); i++) {
+        if (scorer[arr[i][0]] < scorer[arr[i][1]])
+            answer.push_back(arr[i][1]);
+        if (scorer[arr[i][0]] > scorer[arr[i][1]])
+            answer.push_back(arr[i][0]);
+
+        if (scorer[arr[i][0]] == scorer[arr[i][1]]) {
+            string temp = arr[i];
+            sort(temp.begin(), temp.end());
+            answer.push_back(temp[0]);
+        }
     }
 
-    return row * col;
+    return answer;
 }

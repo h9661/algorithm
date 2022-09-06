bca052dfb2d46ba81a5d692dc84efed1cf63b204
h9661 <guscksdn123@gmail.com>
2022-09-06 20:58:00 +0900

    Programmers 같은 숫자는 싫

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,34 +1,24 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-string solution(vector<string> survey, vector<int> choices) {
-    map<char, int> scorer;
+vector<int> solution(vector<int> arr)
+{
+    deque<int> dq;
 
-    for (int i = 0; i < choices.size(); i++) {
-        if (choices[i] > 4) {
-            scorer[survey[i][1]] += choices[i] - 4;
-        }
-        if (choices[i] < 4) {
-            scorer[survey[i][0]] += 4 - choices[i];
-        }
-    }
-
-    string answer = "";
-
-    vector<string> arr = { "RT", "CF", "JM", "AN" };
     for (int i = 0; i < arr.size(); i++) {
-        if (scorer[arr[i][0]] < scorer[arr[i][1]])
-            answer.push_back(arr[i][1]);
-        if (scorer[arr[i][0]] > scorer[arr[i][1]])
-            answer.push_back(arr[i][0]);
-
-        if (scorer[arr[i][0]] == scorer[arr[i][1]]) {
-            string temp = arr[i];
-            sort(temp.begin(), temp.end());
-            answer.push_back(temp[0]);
+        if (dq.empty())
+            dq.push_back(arr[i]);
+        else {
+            if (dq.back() != arr[i])
+                dq.push_back(arr[i]);
+            else
+                continue;
         }
     }
+
+    vector<int> answer;
+    for (int i = 0; i < dq.size(); i++)
+        answer.push_back(dq[i]);
 
     return answer;
 }

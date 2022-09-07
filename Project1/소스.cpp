db639af5a1c3b7bfe83eff21279b0ae07e43d89e
h9661 <guscksdn123@gmail.com>
2022-09-07 19:48:19 +0900

    Programmers 이진 변환 반복하기

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,24 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> solution(vector<int> arr)
-{
-    deque<int> dq;
+vector<int> solution(string s) {
+    vector<int> answer;
 
-    for (int i = 0; i < arr.size(); i++) {
-        if (dq.empty())
-            dq.push_back(arr[i]);
-        else {
-            if (dq.back() != arr[i])
-                dq.push_back(arr[i]);
-            else
-                continue;
+    int binaryTransCount = 0;
+    int removedZeroCount = 0;
+
+    while (s != "1") {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '0')
+                removedZeroCount += 1;
         }
+
+        while (s.find('0') != string::npos) {
+            s.erase(s.begin() + s.find('0'));
+        }
+
+        int lenS = s.size();
+        string newS = "";
+
+        while (lenS != 0) {
+            if (lenS % 2 == 0)
+                newS += '0';
+            else
+                newS += '1';
+
+            lenS /= 2;
+        }
+
+        reverse(newS.begin(), newS.end());
+        s = newS;
+
+        binaryTransCount += 1;
     }
 
-    vector<int> answer;
-    for (int i = 0; i < dq.size(); i++)
-        answer.push_back(dq[i]);
+    answer.push_back(binaryTransCount);
+    answer.push_back(removedZeroCount);
 
     return answer;
 }

096f911774853850feb760951279f6b2ed5163f7
h9661 <guscksdn123@gmail.com>
2022-09-08 14:57:38 +0900

    Programmers 양궁대회

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,42 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> solution(string s) {
-    vector<int> answer;
+int maxValue = -10000;
+vector<int> answer;
 
-    int binaryTransCount = 0;
-    int removedZeroCount = 0;
+void ArrowComb(int count, int index, int n, vector<int>& comb, vector<int>& info, int& arrowN) {
+    if (count == n) {
+        vector<int> arrowArr(11, 0);
+        int remainArrow = arrowN;
 
-    while (s != "1") {
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '0')
-                removedZeroCount += 1;
-        }
-
-        while (s.find('0') != string::npos) {
-            s.erase(s.begin() + s.find('0'));
-        }
-
-        int lenS = s.size();
-        string newS = "";
-
-        while (lenS != 0) {
-            if (lenS % 2 == 0)
-                newS += '0';
+        for (int i = 0; i < 11; i++) {
+            if (comb[i] == 1) {
+                if (remainArrow > info[i]) {
+                    arrowArr[i] = info[i] + 1;
+                    remainArrow -= info[i] + 1;
+                }
+            }
             else
-                newS += '1';
-
-            lenS /= 2;
+                continue;
         }
 
-        reverse(newS.begin(), newS.end());
-        s = newS;
+        if (remainArrow > 0) {
+            arrowArr[10] += remainArrow;
+            remainArrow = 0;
+        }
 
-        binaryTransCount += 1;
+        int lionScore = 0;
+        int apeachScore = 0;
+
+        for (int i = 0; i < 11; i++) {
+            if (info[i] == 0 and arrowArr[i] == 0)
+                continue;
+
+            if (info[i] < arrowArr[i])
+                lionScore += 10 - i;
+            else
+                apeachScore += 10 - i;
+        }
+
+        if (lionScore - apeachScore >= maxValue) {
+            if (lionScore - apeachScore == maxValue) {
+                for (int i = 10; i >= 0; i--) {
+                    if (answer[i] < arrowArr[i]) {
+                        answer = arrowArr;
+                        break;
+                    }
+                    else if (answer[i] == arrowArr[i])
+                        continue;
+                    else
+                        break;
+                }
+            }
+            else {
+                maxValue = lionScore - apeachScore;
+                answer = arrowArr;
+            }
+        }
+
+    }
+    else {
+        for (int i = index; i < 11; i++) {
+            comb[i] = 1;
+
+            ArrowComb(count + 1, index + 1, n, comb, info, arrowN);
+
+            comb[i] = 0;
+        }
+    }
+}
+
+vector<int> solution(int n, vector<int> info) {
+    for (int i = 1; i <= n; i++) {
+        vector<int> comb = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        ArrowComb(0, 0, i, comb, info, n);
     }
 
-    answer.push_back(binaryTransCount);
-    answer.push_back(removedZeroCount);
-
-    return answer;
+    if (maxValue <= 0)
+        return { -1 };
+    else
+        return answer;
 }
